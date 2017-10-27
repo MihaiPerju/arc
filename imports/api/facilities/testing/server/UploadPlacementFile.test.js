bc9ae45212796944a56719e1a8d31a9442821177
MihaiPerju <mihaiperju98@gmail.com>
2017-10-27 14:56:44 +0300

    released test for checking if the array received from csv parsing is translated without errors in an collection object

---
+++ imports/api/facilities/testing/server/UploadPlacementFile.test.js
@@ -0,0 +1,83 @@
+import ParseService from '/imports/api/facilities/server/services/CsvParseService';
+import {chai} from 'meteor/practicalmeteor:chai';
+import shuffle from 'shuffle-array';
+import faker from 'faker';
+
+describe('CSV data to Tasks parser', function () {
+    it("Must parse array extracted from CSV to 'task' data object", function () {
+
+        //Fake Data that normally is received from parsing csv
+        let input;
+
+        //Fake Data that normally is expected to be received from parsing array into object
+        let output;
+
+        //Generated orders, rules and importing rules
+        let orders = [];
+
+        //Array of labels for importing rules
+        let rules = ['acctNum',
+            'facCode',
+            'ptType',
+            'ptName',
+            'dischrgDate',
+            'fbDate',
+            'acctBal',
+            'finClass',
+            'admitDate',
+            'medNo',
+            'insName',
+            'insName2',
+            'insName3',
+            'insCode',
+            'insCode2',
+            'insCode3',
+            'insBal',
+            'insBal2',
+            'insBal3'];
+
+        //generate random order for importing rules
+        generateOrder = () => {
+            return shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+        };
+
+        //generate importing rules based on random order generated before
+        generateImportingRules = () => {
+            let newImportingRules = {};
+            for (i in orders) {
+                newImportingRules[rules[i]] = orders[i];
+            }
+            return newImportingRules;
+        };
+
+        //generate fake array that normally is received by csv parser.
+        generateInput = () => {
+            let newInput = [];
+            for (let i = 0; i < 19; i++) {
+                newInput.push(faker.lorem.word());
+            }
+            return newInput;
+        };
+
+        test = () => {
+            for (key in output) {
+                if (output[key] !== input[importingRules[key] - 1])
+                    return false;
+            }
+            return true;
+        };
+
+        //generate order of rules, importing rules and input test value
+        orders = generateOrder();
+        importingRules = generateImportingRules();
+        input = generateInput();
+
+        //generate output based on input and importingRules
+        output = ParseService.createTask(input, importingRules);
+
+        // assert that test is true
+        chai.assert(
+            test() === true
+        );
+    });
+});
