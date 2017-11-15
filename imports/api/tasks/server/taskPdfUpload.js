56b91736decfe56f7edb0d0b9238ec0c9874d834
MihaiPerju <mihaiperju98@gmail.com>
2017-11-15 20:01:44 +0200

    checked extension of a pdf file

--- imports/api/tasks/server/taskPdfUpload.js
+++ imports/api/tasks/server/taskPdfUpload.js
@@ -2,6 +2,9 @@ import Tasks from '../collection';
 import {createRoute} from '/imports/api/s3-uploads/server/router';
 
 createRoute('/uploads/task-pdf/:taskId', ({taskId, error, filenames, success, upload}) => {
+    if (!filenames[0].endsWith('.pdf')) {
+        return error('Not a valid format');
+    }
     if (filenames.length != 1) {
         return error('Invalid number of files');
     }
