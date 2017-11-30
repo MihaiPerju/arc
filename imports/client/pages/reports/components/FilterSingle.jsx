806d93ab77b7f902370fe9c907ee70a49ce30299
MihaiPerju <mihaiperju98@gmail.com>
2017-11-30 02:35:00 +0200

    created specific input for every data type and integrated it into an autoform. Need logic to combine and create final filter

--- imports/client/pages/reports/components/FilterSingle.jsx
+++ imports/client/pages/reports/components/FilterSingle.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Segment, Button} from 'semantic-ui-react'
+import ReportsService from './../services/ReportsService';
+import {AutoForm, AutoField, ErrorField, SelectField} from 'uniforms-semantic';
 
 export default class FiltersSingle extends React.Component {
     constructor() {
@@ -10,13 +12,57 @@ export default class FiltersSingle extends React.Component {
         this.props.deleteFilter(name);
     }
 
+    getOptions() {
+        const {name, assigneeIdOptions, facilityIdOptions} = this.props;
+        return name === 'assigneeId' ? assigneeIdOptions : facilityIdOptions;
+    }
+
     render() {
-        const {name} = this.props;
+        const {name, keys} = this.props;
 
         return (
             <Segment>
 
                 {name && name.charAt(0).toUpperCase() + name.slice(1)}
+
+                {
+                    ReportsService.isEnum(name, keys) ?
+                        <div>
+                            <AutoField name={name}/>
+                            <ErrorField name={name}/>
+                        </div>
+                        :
+                        ReportsService.isDate(name, keys) ?
+                            <div>
+                                <AutoField name={`${name}Start`}/>
+                                <ErrorField name={`${name}Start`}/>
+
+                                <AutoField name={`${name}End`}/>
+                                <ErrorField name={`${name}End`}/>
+                            </div>
+                            :
+                            ReportsService.isNumber(name, keys) ?
+                                <div>
+                                    <AutoField name={`${name}Start`}/>
+                                    <ErrorField name={`${name}Start`}/>
+
+                                    <AutoField name={`${name}End`}/>
+                                    <ErrorField name={`${name}End`}/>
+                                </div>
+                                :
+                                ReportsService.isLink(name, keys) ?
+                                    <div>
+                                        <SelectField name={name} options={this.getOptions(name)}/>
+                                    </div>
+                                    : <div>
+                                        <AutoField name={name}/>
+                                        <ErrorField name={name}/>
+
+                                        <AutoField name={`${name}Match`}/>
+                                        <ErrorField name={`${name}Match`}/>
+                                    </div>
+
+                }
 
                 <Button onClick={this.deleteFilter.bind(this, name)}
                         floated='right'
--- imports/client/pages/reports/services/ReportsService.js
+++ imports/client/pages/reports/services/ReportsService.js
@@ -22,4 +22,20 @@ export default class ReportsService {
 
         return components;
     }
+
+    static isEnum(name) {
+        return name === 'state' || name === 'substate';
+    }
+
+    static isDate(name) {
+        return name === 'dischrgDate' || name === 'fbDate' || name === 'admitDate';
+    }
+
+    static isNumber(name) {
+        return name === 'medNo' || name === 'insCode' || name === 'insCode2' || name === 'insCode3' || name === 'insBal' || name === 'insBal2' || name === 'insBal3' || name === 'acctBal';
+    }
+
+    static isLink(name) {
+        return name === 'facilityId' || name === 'assigneeId';
+    }
 }
