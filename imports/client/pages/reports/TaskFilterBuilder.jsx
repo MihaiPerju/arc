60a462c4625a5b50560ad16fc10a49ad5bc89bfc
MihaiPerju <mihaiperju98@gmail.com>
2017-11-30 02:35:00 +0200

    created specific input for every data type and integrated it into an autoform. Need logic to combine and create final filter

--- imports/client/pages/reports/TaskFilterBuilder.jsx
+++ imports/client/pages/reports/TaskFilterBuilder.jsx
@@ -4,13 +4,20 @@ import TaskSchema from '/imports/api/tasks/schema';
 import {AutoForm, ErrorField, SelectField} from 'uniforms-semantic';
 import ReportsService from './services/ReportsService';
 import FilterSingle from './components/FilterSingle';
+import FilterBuilderSchema from '/imports/api/tasks/schemas/filterBuilderSchema';
+import Notifier from '/imports/client/lib/Notifier';
+import facilityQuery from '/imports/api/facilities/queries/facilityList';
+import assigneeQuery from '/imports/api/users/queries/listUsers';
 
 export default class TaskFilterBuilder extends React.Component {
     constructor() {
         super();
         this.state = {
             schemaOptions: [],
-            components: {}
+            components: {},
+            keys: [],
+            facilityOptions: [],
+            assigneeOptions: []
         }
     }
 
@@ -25,9 +32,35 @@ export default class TaskFilterBuilder extends React.Component {
         //Creating set of Components based on schema field types
         let components = ReportsService.getComponents(keys);
 
+        //Getting assignee and facility options
+        let facilityOptions = [], assigneeOptions = [];
+
+        facilityQuery.fetch((err, facilities) => {
+            if (!err) {
+                facilities.map((facility) => {
+                    facilityOptions.push({value: facility._id, label: facility._id});
+                });
+                this.setState({facilityOptions});
+            } else {
+                Notifier.error(err.reason);
+            }
+        });
+
+        assigneeQuery.fetch((err, assignees) => {
+            if (!err) {
+                assignees.map((assignee) => {
+                    assigneeOptions.push({value: assignee._id, label: assignee._id});
+                });
+                this.setState({assigneeOptions});
+            } else {
+                Notifier.error(err.reason);
+            }
+        });
+
         this.setState({
             schemaOptions,
-            components
+            components,
+            keys
         });
     }
 
@@ -58,28 +91,47 @@ export default class TaskFilterBuilder extends React.Component {
         });
     }
 
+    onSubmit(data) {
+        console.log(data);
+    }
+
     render() {
-        const {schemaOptions, components} = this.state;
+        const {facilityOptions, assigneeOptions, schemaOptions, components, keys} = this.state;
         return (
             <main className="cc-main">
+                <p/>
+                <p/>
+                <p/>
                 <Container className="page-container">
                     <Header as="h3">Select filters</Header>
                     <Dropdown onChange={this.selectFilter.bind(this)}
                               placeholder="Task fields"
                               options={schemaOptions}/>
-                    {
-                        _.map(components, (item) => {
+                    <AutoForm
+                        schema={FilterBuilderSchema}
+                        onSubmit={this.onSubmit.bind(this)}
+                        ref="form">
 
-                            return item.isActive &&
-                                <FilterSingle
-                                    deleteFilter={this.deleteFilter.bind(this)}
-                                    name={item.name}/>
-                        })
-                    }
+                        {
+                            _.map(components, (item) => {
 
-                    <Button primary fluid>Extract</Button>
+                                return item.isActive &&
+                                    <FilterSingle
+                                        assigneeIdOptions={assigneeOptions}
+                                        facilityIdOptions={facilityOptions}
+                                        deleteFilter={this.deleteFilter.bind(this)}
+                                        keys={keys}
+                                        name={item.name}
+                                    />
+                            })
+                        }
+                        <Button primary fluid type="submit">
+                            Extract
+                        </Button>
+                    </AutoForm>
                 </Container>
             </main>
         )
     }
 }
+
