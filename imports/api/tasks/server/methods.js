043e8c4a46e85357dd192839bb33359a42fe57a4
MihaiPerju <mihaiperju98@gmail.com>
2017-12-13 18:29:30 +0200

    created delete, view and adapted download button

--- imports/api/tasks/server/methods.js
+++ imports/api/tasks/server/methods.js
@@ -1,8 +1,27 @@
-import Security from '/imports/api/security/security.js';
 import ActionService from './services/ActionService.js';
+import Tasks from '../collection';
+import S3 from '/imports/api/s3-uploads/server/s3';
 
 Meteor.methods({
     'task.actions.add'(taskId, actionId) {
         ActionService.createAction(taskId, actionId, this.userId);
+    },
+
+    'task.attachment.remove'(_id, attachmentId, key) {
+        Tasks.update({_id}, {
+            $pull: {
+                attachmentIds: attachmentId
+            }
+        });
+
+        S3.remove(key);
+    },
+
+    'task.attachment.update_order'(_id, attachmentIds) {
+        Tasks.update({_id}, {
+            $set: {
+                attachmentIds
+            }
+        })
     }
 });
--- imports/client/pages/tasks/components/SortableTab.jsx
+++ imports/client/pages/tasks/components/SortableTab.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
-import {path} from '/imports/api/s3-uploads/utils';
+import {path, getToken} from '/imports/api/s3-uploads/utils';
+import {Segment, Button, Divider} from 'semantic-ui-react';
+import Notifier from '/imports/client/lib/Notifier';
 
-const SortableItem = SortableElement(({value}) =>
-    <li>{value}</li>
+const SortableItem = SortableElement(({pdf, index, getPdfName, deletePdf, redirectToPdf}) =>
+    <li style={{'listStyleType': 'none'}}>
+        {
+            pdf &&
+            <Segment clearing>
+                <Button.Group floated='right'>
+                    <Button primary onClick={redirectToPdf.bind(this, pdf)}>
+                        View
+                    </Button>
+                    <Button negative onClick={deletePdf.bind(this, pdf)}>
+                        Delete
+                    </Button>
+                </Button.Group>
+                {getPdfName(pdf)}
+            </Segment>
+        }
+    </li>
 );
 
-const SortableList = SortableContainer(({items}) => {
+const SortableList = SortableContainer(({items, getPdfName, deletePdf, redirectToPdf}) => {
     return (
         <ul>
-            {items.map((value, index) => (
-                <SortableItem key={`item-${index}`} index={index} value={value}/>
+            {items.map((pdf, index) => (
+                <SortableItem redirectToPdf={redirectToPdf}
+                              getPdfName={getPdfName}
+                              deletePdf={deletePdf}
+                              key={`item-${index}`}
+                              index={index}
+                              pdf={pdf}/>
             ))}
         </ul>
     );
 });
+
 export default class SortableTab extends React.Component {
     constructor() {
         super();
@@ -29,19 +52,54 @@ export default class SortableTab extends React.Component {
         });
     };
 
+    downloadPdfs() {
+        const taskId = FlowRouter.current().params._id;
+        const {items} = this.state;
+
+        //creating attachmentIds
+        let attachmentIds = [];
+
+        for (item of items) {
+            attachmentIds.push(item._id);
+        }
+
+        //Updating status in Db
+        Meteor.call('task.attachment.update_order', taskId, attachmentIds, (err) => {
+            if (!err) {
+                Notifier.success('Reordered!');
+                window.open("/pdfs/" + taskId + "/" + getToken(), '_blank');
+            } else {
+                Notifier.error(err.reason);
+            }
+        })
+    }
+
     getPdfName(pdf) {
         return pdf.name.slice(0, pdf.name.indexOf('.'))
+    }
+
+    redirectToPdf(pdf) {
+        window.open(path(pdf.path), '_blank');
+    }
+
+    deletePdf(pdf) {
+        const taskId = FlowRouter.current().params._id;
+
+        Meteor.call('task.attachment.remove', taskId, pdf._id, pdf.path, (err) => {
+            if (!err) {
+                Notifier.success("Attachment removed!");
+            } else {
+                Notifier.error(err.reason);
+            }
+        });
     }
 
     componentWillMount() {
         const {attachments} = this.props;
         let items = [];
-        attachments && attachments.map((pdf, index) => {
+        attachments && attachments.map((pdf) => {
             items.push(
-                <a key={index} target="_blank"
-                   href={path(pdf.path)}>
-                    {this.getPdfName(pdf)}
-                </a>
+                pdf
             )
         });
 
@@ -51,7 +109,18 @@ export default class SortableTab extends React.Component {
     render() {
         return (
             <div>
-                <SortableList items={this.state.items} onSortEnd={this.onSortEnd.bind(this)}/>
+                <SortableList items={this.state.items}
+                              deletePdf={this.deletePdf}
+                              getPdfName={this.getPdfName}
+                              redirectToPdf={this.redirectToPdf}
+                              onSortEnd={this.onSortEnd.bind(this)}/>
+                <Divider/>
+                {
+                    <Button fluid onClick={this.downloadPdfs.bind(this)}
+                            target="_blank">
+                        Download All PDFs
+                    </Button>
+                }
             </div>
         )
     }
