a14cfff2cd1ce83e09bce9d48e32853dfc259864
MihaiPerju <mihaiperju98@gmail.com>
2017-12-20 12:04:15 +0200

    fixed comment 1

--- imports/api/tasks/server/methods.js
+++ imports/api/tasks/server/methods.js
@@ -1,8 +1,10 @@
 import ActionService from './services/ActionService.js';
 import Tasks from '../collection';
 import S3 from '/imports/api/s3-uploads/server/s3';
-import Security from './../security';
-
+import TaskSecurity from './../security';
+import Security from '/imports/api/security/security';
+import RolesEnum from '/imports/api/users/enums/roles';
+import Users from '/imports/api/users/collection';
 
 Meteor.methods({
     'task.actions.add'(taskId, actionId) {
@@ -10,6 +12,9 @@ Meteor.methods({
     },
 
     'task.assignee_change'(data) {
+        TaskSecurity.hasRightsOnTask(this.userId, data.taskId);
+        Security.isAllowed(this.userId, RolesEnum.MANAGER);
+
         Tasks.update({_id: data.taskId}, {
             $set: {
                 assigneeId: data.value
