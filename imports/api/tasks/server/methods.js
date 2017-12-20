5dd0c270bd3cb4b84649b65b48bd01c3a00aaf19
MihaiPerju <mihaiperju98@gmail.com>
2017-12-20 12:48:09 +0200

    changed roles

--- imports/api/tasks/server/methods.js
+++ imports/api/tasks/server/methods.js
@@ -3,7 +3,7 @@ import Tasks from '../collection';
 import S3 from '/imports/api/s3-uploads/server/s3';
 import TaskSecurity from './../security';
 import Security from '/imports/api/security/security';
-import RolesEnum from '/imports/api/users/enums/roles';
+import {roleGroups} from '/imports/api/users/enums/roles';
 import Users from '/imports/api/users/collection';
 
 Meteor.methods({
@@ -13,7 +13,7 @@ Meteor.methods({
 
     'task.assignee_change'(data) {
         TaskSecurity.hasRightsOnTask(this.userId, data.taskId);
-        Security.isAllowed(this.userId, RolesEnum.MANAGER);
+        Security.isAllowed(this.userId, roleGroups.ADMIN_TECH_MANAGER);
 
         Tasks.update({_id: data.taskId}, {
             $set: {
