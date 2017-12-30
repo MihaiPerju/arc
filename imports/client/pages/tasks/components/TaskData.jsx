d9c19a87caea1efd81c36c111e43adf078035553
MihaiPerju <mihaiperju98@gmail.com>
2017-12-30 11:37:18 +0200

    removed pure html and switched to react table

--- imports/client/pages/tasks/components/TaskData.jsx
+++ imports/client/pages/tasks/components/TaskData.jsx
@@ -1,91 +1,93 @@
 import React from 'react';
-import {Container} from 'semantic-ui-react';
+import {Container, Table} from 'semantic-ui-react';
 
 const TaskData = ({task}) => {
     return (
         <Container>
-            <table className='reportTable'>
-                <tr>
-                    <td className='reportTableCell'>AcctNum</td>
-                    <td className='reportTableCell'>{task && task.acctNum}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>FacCode</td>
-                    <td className='reportTableCell'>{task && task.facCode}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>PtType</td>
-                    <td className='reportTableCell'>{task && task.ptType}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>PtName</td>
-                    <td className='reportTableCell'>{task && task.ptName}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>DischrgDate</td>
-                    <td className='reportTableCell'>{task && task.dischrgDate}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>FbDate</td>
-                    <td className='reportTableCell'>{task && task.fbDate}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>AcctBal</td>
-                    <td className='reportTableCell'> {task && task.acctBal}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>FinClass</td>
-                    <td className='reportTableCell'>{task && task.finClass}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>AdmitDate</td>
-                    <td className='reportTableCell'>{task && task.admitDate}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>MedNo</td>
-                    <td className='reportTableCell'>{task && task.medNo}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsName</td>
-                    <td className='reportTableCell'>{task && task.insName}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsName2</td>
-                    <td className='reportTableCell'>{task && task.insName2}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsName3</td>
-                    <td className='reportTableCell'>{task && task.insName3}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsCode</td>
-                    <td className='reportTableCell'>{task && task.insCode}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsCode2</td>
-                    <td className='reportTableCell'>{task && task.insCode2}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsCode3</td>
-                    <td className='reportTableCell'>{task && task.insCode3}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsBal</td>
-                    <td className='reportTableCell'>{task && task.insBal}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsBal2</td>
-                    <td className='reportTableCell'>{task && task.insBal2}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>InsBal3</td>
-                    <td className='reportTableCell'>{task && task.insBal3}</td>
-                </tr>
-                <tr>
-                    <td className='reportTableCell'>State</td>
-                    <td className='reportTableCell'>{task && task.state}</td>
-                </tr>
-            </table>
+            <Table textAlign="center" celled>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>AcctNum</Table.Cell>
+                        <Table.Cell>{task && task.acctNum}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>FacCode</Table.Cell>
+                        <Table.Cell>{task && task.facCode}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>PtType</Table.Cell>
+                        <Table.Cell>{task && task.ptType}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>PtName</Table.Cell>
+                        <Table.Cell>{task && task.ptName}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>DischrgDate</Table.Cell>
+                        <Table.Cell>{task && task.dischrgDate}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>FbDate</Table.Cell>
+                        <Table.Cell>{task && task.fbDate}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>AcctBal</Table.Cell>
+                        <Table.Cell> {task && task.acctBal}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>FinClass</Table.Cell>
+                        <Table.Cell>{task && task.finClass}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>AdmitDate</Table.Cell>
+                        <Table.Cell>{task && task.admitDate}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>MedNo</Table.Cell>
+                        <Table.Cell>{task && task.medNo}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsName</Table.Cell>
+                        <Table.Cell>{task && task.insName}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsName2</Table.Cell>
+                        <Table.Cell>{task && task.insName2}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsName3</Table.Cell>
+                        <Table.Cell>{task && task.insName3}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsCode</Table.Cell>
+                        <Table.Cell>{task && task.insCode}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsCode2</Table.Cell>
+                        <Table.Cell>{task && task.insCode2}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsCode3</Table.Cell>
+                        <Table.Cell>{task && task.insCode3}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsBal</Table.Cell>
+                        <Table.Cell>{task && task.insBal}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsBal2</Table.Cell>
+                        <Table.Cell>{task && task.insBal2}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>InsBal3</Table.Cell>
+                        <Table.Cell>{task && task.insBal3}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>State</Table.Cell>
+                        <Table.Cell>{task && task.state}</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
         </Container>
     )
 };
