a107bfe4a495e9d11fab74503cc20ff6f543b7c5
MihaiPerju <mihaiperju98@gmail.com>
2017-12-30 12:29:39 +0200

    changed schedule service and finished functionality

--- imports/api/reports/services/ScheduleService.js
+++ imports/api/reports/services/ScheduleService.js
@@ -1,103 +1,19 @@
 import React from 'react'
-import reactElementToJSXString from 'react-element-to-jsx-string';
 import Notifier from '/imports/client/lib/Notifier';
+import TaskData from '/imports/client/pages/tasks/components/TaskData';
+import ReactDOMServer from 'react-dom/server';
 
 export default class ScheduleService {
     static createReportPdf(userIds, tasks, report) {
 
         let ReportTable = '';
         for (task of tasks) {
-            const taskSingle = <div>
-                <table>
-                    <tr>
-                        <td>AcctNum</td>
-                        <td>{task && task.acctNum}</td>
-                    </tr>
-                    <tr>
-                        <td>FacCode</td>
-                        <td>{task && task.facCode}</td>
-                    </tr>
-                    <tr>
-                        <td>PtType</td>
-                        <td>{task && task.ptType}</td>
-                    </tr>
-                    <tr>
-                        <td>PtName</td>
-                        <td>{task && task.ptName}</td>
-                    </tr>
-                    <tr>
-                        <td>DischrgDate</td>
-                        <td>{task && task.dischrgDate}</td>
-                    </tr>
-                    <tr>
-                        <td>FbDate</td>
-                        <td>{task && task.fbDate}</td>
-                    </tr>
-                    <tr>
-                        <td>AcctBal</td>
-                        <td> {task && task.acctBal}</td>
-                    </tr>
-                    <tr>
-                        <td>FinClass</td>
-                        <td>{task && task.finClass}</td>
-                    </tr>
-                    <tr>
-                        <td>AdmitDate</td>
-                        <td>{task && task.admitDate}</td>
-                    </tr>
-                    <tr>
-                        <td>MedNo</td>
-                        <td>{task && task.medNo}</td>
-                    </tr>
-                    <tr>
-                        <td>InsName</td>
-                        <td>{task && task.insName}</td>
-                    </tr>
-                    <tr>
-                        <td>InsName2</td>
-                        <td>{task && task.insName2}</td>
-                    </tr>
-                    <tr>
-                        <td>InsName3</td>
-                        <td>{task && task.insName3}</td>
-                    </tr>
-                    <tr>
-                        <td>InsCode</td>
-                        <td>{task && task.insCode}</td>
-                    </tr>
-                    <tr>
-                        <td>InsCode2</td>
-                        <td>{task && task.insCode2}</td>
-                    </tr>
-                    <tr>
-                        <td>InsCode3</td>
-                        <td>{task && task.insCode3}</td>
-                    </tr>
-                    <tr>
-                        <td>InsBal</td>
-                        <td>{task && task.insBal}</td>
-                    </tr>
-                    <tr>
-                        <td>InsBal2</td>
-                        <td>{task && task.insBal2}</td>
-                    </tr>
-                    <tr>
-                        <td>InsBal3</td>
-                        <td>{task && task.insBal3}</td>
-                    </tr>
-                    <tr>
-                        <td>State</td>
-                        <td>{task && task.state}</td>
-                    </tr>
-                </table>
-                <p/>
-            </div>;
+            const taskSingle = <TaskData task={task}/>;
 
-            ReportTable += reactElementToJSXString(taskSingle);
+            ReportTable += ReactDOMServer.renderToString(taskSingle) + '<br/>';
         }
 
-        const htmlString = reactElementToJSXString(ReportTable);
-        Meteor.pdf.stream(htmlString, function (result) {
+        Meteor.pdf.stream(ReportTable, function (result) {
             ScheduleService.sendEmails(userIds, result, report);
             Notifier.success("Emails sent!");
         });
@@ -130,6 +46,5 @@ export default class ScheduleService {
                 }
             })
         }
-
     }
 }
