ee476a06646c5eba40c4d3e7c406a16bd09e1299
MihaiPerju <mihaiperju98@gmail.com>
2018-01-08 16:11:19 +0200

    minimalised action buttons on posts

--- imports/client/pages/posts/components/PostSingle.jsx
+++ imports/client/pages/posts/components/PostSingle.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Notifier from '/imports/client/lib/Notifier';
 import {Icon, Label, Menu, Table} from 'semantic-ui-react'
-import {Button} from 'semantic-ui-react'
+import {Button, Dropdown} from 'semantic-ui-react'
 
 export default class PostSingle extends Component {
     deletePost() {
@@ -31,10 +31,14 @@ export default class PostSingle extends Component {
                 <Table.Cell>{post.user.emails[0].address}</Table.Cell>
                 <Table.Cell>
                     {this.isMyPost() ?
-                        <Button.Group>
-                            <Button primary href={"/post/" + post._id + "/edit"}>Edit</Button>
-                            <Button color="red" onClick={this.deletePost.bind(this)}>Delete</Button>
-                        </Button.Group>
+                        <Dropdown button text='Action' icon={null}>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>
+                                    <Button primary href={"/post/" + post._id + "/edit"}>Edit</Button>
+                                    <Button color="red" onClick={this.deletePost.bind(this)}>Delete</Button>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                         :
                         null
                     }
