61f65bfc2c5e671b002b628693426f2ea68c5fd5
MihaiPerju <mihaiperju98@gmail.com>
2018-01-11 23:25:00 +0200

    clearing + fixing issues

--- imports/cronjobs/index.js
+++ imports/cronjobs/index.js
@@ -1,6 +1,4 @@
 import './cron';
-import SftpTransport from './SftpTransport';
-import FacilityFileScan from './server/FacilityFileScan';
 
-const fileScan = new FacilityFileScan();
-fileScan.run();
+//start cronometer
+SyncedCron.start();
