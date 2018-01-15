3bf051111b5486fbc725af4dc23ccd54705f9519
MihaiPerju <mihaiperju98@gmail.com>
2018-01-15 22:18:58 +0200

    fixed and released second functionality - processing

--- imports/api/facilities/server/services/TaskImportingService.js
+++ imports/api/facilities/server/services/TaskImportingService.js
@@ -2,15 +2,14 @@ import CsvParseService from './CsvParseService';
 import Tasks from '/imports/api/tasks/collection';
 
 export default class TaskService {
-
     //For placement file
     static upload(results, importRules, facilityId) {
         const tasks = CsvParseService.convertToTasks(results, importRules, true, facilityId);
         //Creating tasks
         tasks.map((task) => {
-            Tasks.insert(task);
+            const RowTasks = Tasks.rawCollection();
+            RowTasks.insert(task);
         });
-
     }
 
     //For inventory file
