958d9ebe72616cc2cd1ce88989b193f9f9afb5d9
MihaiPerju <mihaiperju98@gmail.com>
2018-01-22 16:02:59 +0200

    created dashboard

--- imports/client/pages/home/components/Dashboard.jsx
+++ imports/client/pages/home/components/Dashboard.jsx
@@ -1,30 +1,69 @@
 import React from 'react';
 import {Container, Card, Icon} from 'semantic-ui-react'
 import {Pie} from 'react-chartjs';
+import query from '/imports/api/tasks/queries/taskList';
+import Notifier from '/imports/client/lib/Notifier';
+import StateEnum from '/imports/api/tasks/enums/states';
 
-const pieData = [
-    {
-        value: 300,
-        color: "#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
+const chartColors = {
+    ACTIVE: {
+        color: "#147087",
+        highlight: "#1889A6"
     },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
+    ARCHIVED: {
+        color: "#57B81E",
+        highlight: "#1adb4a"
     },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
+    HOLD: {
+        color: "#363636",
+        highlight: "#5A5A5A",
     }
-];
-
+}
 export default class Dashboard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tasks: [],
+            chartData: []
+        }
+    }
+
+    componentWillMount() {
+        query.fetch((err, tasks) => {
+            if (!err) {
+                this.setState({
+                    tasks
+                });
+                for (key in StateEnum) {
+                    let {chartData} = this.state;
+                    console.log(key, StateEnum[key], chartColors[key].color);
+                    chartData.push({
+                        value: this.countAccounts(StateEnum[key]),
+                        color: chartColors[key].color,
+                        highlight: chartColors[key].highlight,
+                        label: StateEnum[key]
+                    })
+                    this.setState({chartData});
+                }
+            } else {
+                Notifier.error(err.reason);
+            }
+        });
+    }
+
+    countAccounts(state) {
+        const {tasks} = this.state;
+        let count = 0;
+        tasks.forEach((task) => {
+            if (task.state === state) {
+                count++;
+            }
+        });
+        return count;
+    }
+
     render() {
+        const {chartData} = this.state;
         return (
             <Container>
                 <Card fluid>
@@ -33,13 +72,13 @@ export default class Dashboard extends React.Component {
                             Dashboard
                         </Card.Header>
                         <Card.Description style={{"textAlign": "left"}}>
-                            <p>- Total Accounts On Hold: X</p>
-                            <p>- Total Accounts Active: X</p>
-                            <p>- Total Accounts Archived: X</p>
+                            <p>- Total Accounts On Hold: {this.countAccounts(StateEnum.HOLD)}</p>
+                            <p>- Total Accounts Active: {this.countAccounts(StateEnum.ACTIVE)}</p>
+                            <p>- Total Accounts Archived: {this.countAccounts(StateEnum.ARCHIVED)}</p>
                             <p>- Total Accounts This Month: X</p>
                             <p>- Total Accounts This Week: X</p>
                         </Card.Description>
-                        <Pie data={pieData}/>
+                        <Pie data={chartData}/>
                     </Card.Content>
                 </Card>
             </Container>
