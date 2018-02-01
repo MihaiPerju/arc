1af1904fda3f5ee4cfabcb8f7f880c115e4c6c6d
MihaiPerju <mihaiperju98@gmail.com>
2018-02-01 09:13:01 +0200

    added test for importing placement file with header

---
+++ imports/api/facilities/testing/server/UploadHeaderPlacementFile.test.js
@@ -0,0 +1,88 @@
+import TaskService from '/imports/api/facilities/server/services/TaskImportingService';
+import {chai} from 'meteor/practicalmeteor:chai';
+import Tasks from '/imports/api/tasks/collection';
+import {resetDatabase} from 'meteor/xolvio:cleaner';
+
+describe('Import header placement files', function () {
+
+    beforeEach(function () {
+        resetDatabase();
+    });
+
+    it("Must update old documents and insert new documents", function () {
+
+        const input = [
+            ['Account Number', 'Fac Code', 'Pt Type', 'Pt Name', 'Discharge Date', 'Fb Date',
+                'Account Balance', 'Fin Class', 'Admit Date', 'Med No', 'Ins Name', 'Ins Name 2',
+                'Ins Name 3', 'Ins Code', 'Ins Code 2', 'Ins Code 3', 'Modified', 'Ins Balance 2',
+                'Ins Balance 3', 'Meta1', 'Meta2']
+                ['AcnxX49kFFBTDxF5m', 'M', 'M', '4', '01/02/2018', '12/02/2015', '7', '8', '12/02/2015',
+                '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', 'metavalue1', 'metavalue2']
+                ['AcnxX49kFFBTDxF5m', '2', '1', '1', '10/08/2018', '12/02/2015', '1', '1', '12/02/2015', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1', 'metavalue3', 'metavalue4']
+        ];
+
+        const importRules = {
+            "hasHeader": true,
+            "acctNum": "Account Number",
+            "facCode": "Fac Code",
+            "ptType": "Pt Type",
+            "ptName": "Pt Name",
+            "dischrgDate": "Discharge Date",
+            "fbDate": "Fb Date",
+            "acctBal": "Account Balance",
+            "finClass": "Fin Class",
+            "admitDate": "Admit Date",
+            "medNo": "Med No",
+            "insName": "Ins Name",
+            "insName2": "Ins Name 2",
+            "insName3": "Ins Name 3",
+            "insCode": "Ins Code",
+            "insCode2": "Ins Code 2",
+            "insCode3": "Ins Code 3",
+            "insBal": "Modified",
+            "insBal2": "Ins Balance 2",
+            "insBal3": "Ins Balance 3"
+        };
+
+
+        test = () => {
+            const expectedTask1 = {
+                "facilityId": "7QeQq4fDgm7miDNe6",
+                "acctNum": "AcnxX49kFFBTDxF5m",
+                "facCode": "M",
+                "ptType": "M",
+                "ptName": "4",
+                "dischrgDate": ISODate("2018-01-01T22:00:00Z"),
+                "fbDate": ISODate("2015-12-01T22:00:00Z"),
+                "acctBal": 7,
+                "finClass": "8",
+                "admitDate": ISODate("2015-12-01T22:00:00Z"),
+                "medNo": 10,
+                "insName": "11",
+                "insName2": "12",
+                "insName3": "13",
+                "insCode": 14,
+                "insCode2": 15,
+                "insCode3": 16,
+                "insBal": 17,
+                "insBal2": 18,
+                "insBal3": 19,
+                "metaData": {
+                    "Meta1": "metavalue1",
+                    "Meta2": "metavalue2"
+                },
+                "createdAt": ISODate("2018-01-31T16:05:51.807Z"),
+                "state": "Active",
+                "substate": "new"
+            };
+
+            return true;
+        };
+
+        // assert that test is true
+        chai.assert(
+            test() === true
+        );
+    });
+});
