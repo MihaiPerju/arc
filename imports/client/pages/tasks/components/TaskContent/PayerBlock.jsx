300ca17e47d1ecdb8fce8fa8da8af7e0be358e84
maxon <max.shmakov9@gmail.com>
2018-02-06 19:03:26 +0200

    upload

--- imports/client/pages/tasks/components/TaskContent/PayerBlock.jsx
+++ imports/client/pages/tasks/components/TaskContent/PayerBlock.jsx
@@ -6,31 +6,53 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default class PayerBlock extends Component {
-	render() {
-		var settings = {
-			infinite: true,
-			speed: 500,
-			slidesToShow: 4,
-			slidesToScroll: 1
-	    };
-		return (
-			<div className="action-block">
-				<div className="header__block">
-					<div className="title-block text-uppercase">Payer Breakdown</div>
-				</div>
-				<div className="main__block">
-					<div className="slide-payers">
-						<Slider {...settings}>
-							<div><PayItem/></div>
-							<div><PayItem/></div>
-							<div><PayItem/></div>
-							<div><PayItem/></div>
-							<div><PayItem/></div>
-							<div><PayItem/></div>
-						</Slider>
-					</div>
-				</div>
-			</div>
-		)
-	}
+    render() {
+        var settings = {
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,            
+            nextArrow: <RightArrow/>,
+            prevArrow: <LeftArrow/>,
+            slidesToScroll: 1
+        };
+        return (
+            <div className="action-block">
+                <div className="header__block">
+                    <div className="title-block text-uppercase">Payer Breakdown</div>
+                </div>
+                <div className="main__block">
+                    <div className="slide-payers">
+                        <Slider {...settings}>
+                            <div><PayItem/></div>
+                            <div><PayItem/></div>
+                            <div><PayItem/></div>
+                            <div><PayItem/></div>
+                            <div><PayItem/></div>
+                            <div><PayItem/></div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class RightArrow extends Component {
+    render() {
+        return (
+            <div onClick={this.props.onClick} className="btn-next">
+                <i className="icon-angle-right"/>
+            </div>
+        )
+    }
+}
+
+class LeftArrow extends Component {
+    render() {
+        return (
+            <div onClick={this.props.onClick} className="btn-prev">
+                <i className="icon-angle-left"/>
+            </div>
+        )
+    }
 }
