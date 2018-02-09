ba3d76b77f8c34680f6fa7dca4ba1acec5f21826
MihaiPerju <mihaiperju98@gmail.com>
2018-02-09 17:22:46 +0200

    created test for checking upload of a placement csv file with header

--- imports/api/facilities/testing/server/UploadHeaderPlacementFile.test.js
+++ imports/api/facilities/testing/server/UploadHeaderPlacementFile.test.js
@@ -9,17 +9,16 @@ describe('Import header placement files', function () {
         resetDatabase();
     });
 
-    it("Must update old documents and insert new documents", function () {
+    it("Must create correct accounts with Metadata", function () {
 
         const input = [
             ['Account Number', 'Fac Code', 'Pt Type', 'Pt Name', 'Discharge Date', 'Fb Date',
                 'Account Balance', 'Fin Class', 'Admit Date', 'Med No', 'Ins Name', 'Ins Name 2',
                 'Ins Name 3', 'Ins Code', 'Ins Code 2', 'Ins Code 3', 'Modified', 'Ins Balance 2',
                 'Ins Balance 3', 'Meta1', 'Meta2'],
-                ['AcnxX49kFFBTDxF5m', 'M', 'M', '4', '01/02/2018', '12/02/2015', '7', '8', '12/02/2015',
+            ['AcnxX49kFFBTDxF5m', 'M', 'M', '4', '01/02/2018', '12/02/2015', '7', '8', '12/02/2015',
                 '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', 'metavalue1', 'metavalue2'],
-                ['AcnxX49kFFBTDxF5m', '2', '1', '1', '10/08/2018', '12/02/2015', '1', '1', '12/02/2015', '1',
-                '1', '1', '1', '1', '1', '1', '1', '1', '1', 'metavalue3', 'metavalue4']
+            ''
         ];
 
         const importRules = {
@@ -44,42 +43,63 @@ describe('Import header placement files', function () {
             "insBal2": "Ins Balance 2",
             "insBal3": "Ins Balance 3"
         };
+        const facilityId = 'abcdefghijklmnop';
+        TaskService.upload(input, importRules, facilityId);
+        const accounts = Tasks.find().fetch();
+        const account = accounts[0];
+
+        //Delete date fields + id for easy checking
+        delete account.fbDate;
+        delete account.admitDate;
+        delete account.dischrgDate;
+        delete account.createdAt;
+        delete account._id;
+
+
+        const expectedAccount = {
+            facilityId: 'abcdefghijklmnop',
+            acctNum: 'AcnxX49kFFBTDxF5m',
+            facCode: 'M',
+            ptType: 'M',
+            ptName: 4,
+            acctBal: 7,
+            finClass: 8,
+            medNo: 10,
+            insName: 11,
+            insName2: 12,
+            insName3: 13,
+            insCode: 14,
+            insCode2: 15,
+            insCode3: 16,
+            insBal: 17,
+            insBal2: 18,
+            insBal3: 19,
+            metaData: {
+                Meta1: 'metavalue1',
+                Meta2: 'metavalue2'
+            },
+            state: 'Active',
+            substate: 'new'
+        };
 
 
         test = () => {
-            const expectedTask1 = {
-                "facilityId": "7QeQq4fDgm7miDNe6",
-                "acctNum": "AcnxX49kFFBTDxF5m",
-                "facCode": "M",
-                "ptType": "M",
-                "ptName": "4",
-                "acctBal": 7,
-                "finClass": "8",
-                "medNo": 10,
-                "insName": "11",
-                "insName2": "12",
-                "insName3": "13",
-                "insCode": 14,
-                "insCode2": 15,
-                "insCode3": 16,
-                "insBal": 17,
-                "insBal2": 18,
-                "insBal3": 19,
-                "metaData": {
-                    "Meta1": "metavalue1",
-                    "Meta2": "metavalue2"
-                },
-                "state": "Active",
-                "substate": "new"
-            };
-
+            if (JSON.stringify(account.metaData) !== JSON.stringify(expectedAccount.metaData)){
+                return false;
+            }
+            delete account.metaData;
+            delete expectedAccount.metaData;
+            for (key in account) {
+                if (account[key] != expectedAccount[key]) {
+                    return false;
+                }
+            }
             return true;
         };
 
         // assert that test is true
         chai.assert(
-            // test() === true
-            1 === 1
+            test() === true
         );
     });
 });
