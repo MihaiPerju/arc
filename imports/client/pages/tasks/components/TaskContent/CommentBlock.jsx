c1994779c753f72e5186de849146819613c84fad
maxon <max.shmakov9@gmail.com>
2018-02-09 18:22:48 +0200

    upload

--- imports/client/pages/tasks/components/TaskContent/CommentBlock.jsx
+++ imports/client/pages/tasks/components/TaskContent/CommentBlock.jsx
@@ -7,6 +7,23 @@ export default class LetterList extends Component {
                 <div className="header__block">
                     <div className="title-block text-uppercase">Comments</div>
                 </div>               
+                <form action="" className="comment-block">
+                    <div className="form-group">
+                        <img className="md-avatar img-circle" src="/assets/img/user.svg" alt=""/>                          
+                        <div contentEditable="true" className="text-area" placeholder="Leave you comment"></div>
+                        <button className="btn-post">Post</button>
+                    </div>
+                </form>
+                <div className="comment-list">
+                    <div className="comment-item">
+                        <div className="comment__wrapper">
+                            <img className="md-avatar img-circle" src="/assets/img/user.svg" alt=""/>
+                            <div className="name">Beneon Dream</div>
+                            <div className="message text-light-grey">Everything looks ready for work dont forget that!</div>
+                        </div>
+                        <div className="time">11:20</div>
+                    </div>
+                </div>
             </div>
         )
     }
--- imports/client/pages/tasks/components/TaskContent/LetterList.jsx
+++ imports/client/pages/tasks/components/TaskContent/LetterList.jsx
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
+import NewLetter from './NewLetter';
 
 export default class LetterList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            createLetter: false
+        }
+        this.newLetter = this.newLetter.bind(this);
+    }
+    
+    newLetter() {
+        this.setState({
+            createLetter: !this.state.createLetter
+        })
+    }
+
     render() {
         return (
             <div className="action-block">
@@ -8,9 +23,23 @@ export default class LetterList extends Component {
                     <div className="title-block text-uppercase">Letter list</div>
                 </div>
                 <div className="main__block">
-                    <div className="add-content">
+                    <div className="add-content" onClick={this.newLetter}>
                         <i className="icon-envelope-o"/>
                         <div className="text-center">+ Create a new letter</div>
+                    </div>
+                    { this.state.createLetter ? <NewLetter/> : null }                    
+                    <div className="block-list letter-list">
+                        <div className="block-item">
+                            <div className="info">
+                                <div className="title">Contact client with offers</div>
+                                <div className="status pending">Pending</div>
+                            </div>
+                            <div className="btn-group">
+                                <button className="btn-text--blue"><i className="icon-download"/></button>
+                                <button className="btn-text--red"><i className="icon-trash-o"/></button>
+                                <button className="btn--blue">View</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
