35003bdd13732652dd8ebdf0233dff387442cbcd
MihaiPerju <mihaiperju98@gmail.com>
2018-02-16 09:13:17 +0200

    started adding actions

--- imports/client/pages/tasks/components/TaskContent/NewAction.jsx
+++ imports/client/pages/tasks/components/TaskContent/NewAction.jsx
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
 import SelectBlock from '/imports/client/lib/SelectBlock.jsx';
+import {AutoForm} from 'uniforms-semantic';
+import SimpleSchema from "simpl-schema";
+import SelectSimple from "/imports/client/lib/uniforms/SelectSimple.jsx";
+import {ErrorField} from '/imports/';
+
+const ActionSchema = new SimpleSchema({
+    action: {
+        type: String,
+        optional: true
+    }
+});
 
 export default class NewAction extends Component {
     constructor() {
@@ -8,7 +19,7 @@ export default class NewAction extends Component {
             fade: false
         }
     }
-    
+
     componentDidMount() {
         setTimeout(() => {
             this.setState({fade: true});
@@ -22,6 +33,10 @@ export default class NewAction extends Component {
                     <img className="md-avatar img-circle" src="/assets/img/user1.svg" alt=""/>
                     <div className="name">Solomon Ben</div>
                 </div>
+                <AutoForm schema={ActionSchema} onSubmit={this.onSubmit} ref="form">
+                    <SelectSimple name="action" options={[{value: 1, label: 1},{value:2,label:2}]}/>
+                    <ErrorField name="action"/>
+                </AutoForm>
                 <form action="" className="action-form">
                     <SelectBlock header={'Select action'}/>
                     <div className="form-group">
