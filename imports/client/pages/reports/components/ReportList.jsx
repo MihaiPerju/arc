e3094b050650805971419462c5ca8c6d436950a0
MihaiPerju <mihai.perju@cultofcoders.com>
2018-02-21 01:21:49 +0200

    adapted reports to new ui

--- imports/client/pages/reports/components/ReportList.jsx
+++ imports/client/pages/reports/components/ReportList.jsx
@@ -3,27 +3,24 @@ import ReportSingle from './ReportSingle';
 
 export default class ReportList extends Component {
     render() {
-    	const reports = [
-			{ title: 'Needs a new upgrade, Report nr' },
-			{ title: 'Report nr' },
-			{ title: 'Unknown cliend, needs to be checked' },
-    	];
-    	const reportList = reports.map(function(report, index){
-    		const { renderContent, showBtnGroup } = this.props;
-    		return (
-				<ReportSingle
-					key={index}
-					id={index}
-					title={report.title}
-					renderContent={renderContent}
-					showBtnGroup={showBtnGroup}
-				/>
-    		)
-    	}, this);
-    	
+        const {reports} = this.props;
+        const reportList = reports.map(function (report, index) {
+            const {setReport, selectReport, reportsSelected, currentReport} = this.props;
+            return (
+                <ReportSingle
+                    reportsSelected={reportsSelected}
+                    currentReport={currentReport}
+                    selectReport={selectReport}
+                    setReport={setReport}
+                    report={report}
+                    key={index}
+                />
+            )
+        }, this);
+
         return (
             <div className={this.props.class}>
-                { reportList }
+                {reportList}
             </div>
         );
     }
