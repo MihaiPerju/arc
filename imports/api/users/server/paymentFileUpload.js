2ee3bbde69f0c71f822fdafc105457722f2492ab
MihaiPerju <mihai.perju@cultofcoders.com>
2018-02-22 12:47:42 +0200

    created route and added posibility to upload payment file

---
+++ imports/api/users/server/paymentFileUpload.js
@@ -0,0 +1,30 @@
+import {createRoute} from '/imports/api/s3-uploads/server/router';
+import Papa from 'papaparse';
+import fs from 'fs';
+import ParseService from '/imports/api/facilities/server/services/CsvParseService';
+import TaskService from '/imports/api/facilities/server/services/TaskImportingService';
+
+createRoute('/uploads/payment/:facilityId', ({facilityId, error, filenames, success}) => {
+
+    if (filenames.length != 1) {
+        return error('Invalid number of files');
+    }
+    console.log(facilityId);
+    console.log("Entered!");
+    // const importRules = ParseService.getImportRules(facilityId,'inventoryRules');
+    //
+    // for (index in filenames) {
+    //     const stream = fs.readFileSync(filenames[index]);
+    //     const csvString = stream.toString();
+    //
+    //     Papa.parse(csvString, {
+    //             chunk: (results) => {
+    //                 // the result needs to be performed here
+    //                 TaskService.update(results.data, importRules, facilityId);
+    //             }
+    //         }
+    //     );
+    // }
+
+    success();
+});
