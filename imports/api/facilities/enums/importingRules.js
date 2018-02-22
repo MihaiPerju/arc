edf191927caef44fef8e97d17eca9b82eb808bae
MihaiPerju <mihai.perju@cultofcoders.com>
2018-02-22 12:36:37 +0200

    implemented payment files importing rules

--- imports/api/facilities/enums/importingRules.js
+++ imports/api/facilities/enums/importingRules.js
@@ -1,19 +1,36 @@
+let commonRules = [
+    {value: 'acctNum', label: 'Account Number', optional: true},
+    {value: 'facCode', label: 'Facility Code', optional: true},
+    {value: 'ptType', label: 'Pt Type', optional: true},
+    {value: 'ptName', label: 'Pt Name', optional: true},
+    {value: 'dischrgDate', label: 'Discharge Date', optional: true},
+    {value: 'fbDate', label: 'Fb Date', optional: true},
+    {value: 'acctBal', label: 'Acct Balance', optional: true},
+    {value: 'finClass', label: 'Fin Class', optional: true},
+    {value: 'admitDate', label: 'Admit Date', optional: true},
+    {value: 'medNo', label: 'Med No', optional: true}
+];
+
 export default {
-    importingRules: [
-        {value: 'acctNum', label: 'Account Number'},
-        {value: 'facCode', label: 'Facility Code'},
-        {value: 'ptType', label: 'Pt Type'},
-        {value: 'ptName', label: 'Pt Name'},
-        {value: 'dischrgDate', label: 'Discharge Date'},
-        {value: 'fbDate', label: 'Fb Date'},
-        {value: 'acctBal', label: 'Acct Balance'},
-        {value: 'finClass', label: 'Fin Class'},
-        {value: 'admitDate', label: 'Admit Date'},
-        {value: 'medNo', label: 'Med No'}
+    inventoryRules: commonRules,
+    placementRules: commonRules,
+    paymentRules: [
+        {value: 'ptName', label: 'Patient Name', optional: true},
+        {value: 'acctNum', label: 'Account Number', optional: false},
+        {value: 'admitDate', label: 'Admit Date', optional: true},
+        {value: 'dischrgDate', label: 'Discharge Date', optional: true},
+        {value: 'payorCode', label: 'Payor Code', optional: false},
+        {value: 'insName', label: 'Insurance Name', optional: true},
+        {value: 'newAcctBal', label: 'New Account Balance', optional: true},
+        {value: 'ptBalance', label: 'Payment Balance', optional: true},
+        {value: 'finClass', label: 'Financial Class', optional: true},
+        {value: 'transDate', label: 'Transaction Date', optional: false},
+        {value: 'transType', label: 'Transaction Type', optional: false},
+        {value: 'transAmount', label: 'Transaction Amount', optional: false}
     ],
     types: {
-        dates: ['dischrgDate', 'fbDate', 'admitDate'],
-        numbers: ['acctBal', 'medNo', 'insCode','insBal'],
-        strings: ['acctNum', 'ptType', 'ptName', 'finClass', 'insName', 'facCode'],
+        dates: ['dischrgDate', 'fbDate', 'admitDate', 'transDate'],
+        numbers: ['acctBal', 'medNo', 'insCode', 'insBal', 'newAcctBal', 'ptBalance', 'transAmount'],
+        strings: ['transType', 'acctNum', 'payorCode', 'ptType', 'ptName', 'finClass', 'insName', 'facCode'],
     }
 }
