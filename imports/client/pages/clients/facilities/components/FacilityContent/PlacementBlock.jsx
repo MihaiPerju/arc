eb3e9e7261b91d6ba673834e55f211d42f9c96fe
MihaiPerju <mihai.perju@cultofcoders.com>
2018-02-25 13:59:06 +0200

    included importing rules

--- imports/client/pages/clients/facilities/components/FacilityContent/PlacementBlock.jsx
+++ imports/client/pages/clients/facilities/components/FacilityContent/PlacementBlock.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import UploadItem from './UploadItem';
 import Notifier from "../../../../../lib/Notifier";
 import DropzoneComponent from 'react-dropzone-component';
+import ImportingRules from '../ImportingRules';
 
 export default class PlacementBlock extends Component {
     render() {
@@ -29,35 +30,12 @@ export default class PlacementBlock extends Component {
                         <DropzoneComponent config={componentConfig} djsConfig={djsConfig}/>
                     </div>
                 </div>
-                <UploadForm/>
-            </div>
-        )
-    }
-}
-
-class UploadForm extends Component {
-    render() {
-        return (
-            <div className="upload-section">
-                {/*<form action="">*/}
-                    {/*<div className="form-wrapper">*/}
-                        {/*<div className="radio-group">*/}
-                            {/*<h6>File with header</h6>*/}
-                            {/*<div className="radio-item">*/}
-                                {/*<input type="radio" id="r1" name="header"/>*/}
-                                {/*<label htmlFor="r1">True</label>*/}
-                            {/*</div>*/}
-                            {/*<div className="radio-item">*/}
-                                {/*<input type="radio" id="r2" name="header"/>*/}
-                                {/*<label htmlFor="r2">False</label>*/}
-                            {/*</div>*/}
-                        {/*</div>*/}
-                        {/*<div className="btn-group">*/}
-                            {/*<button className="btn--red">Cancel</button>*/}
-                            {/*<button className="btn--green">Submit</button>*/}
-                        {/*</div>*/}
-                    {/*</div>*/}
-                {/*</form>*/}
+                <div className="upload-section">
+                    <ImportingRules rules={"placementRules"} model={facility}/>
+                </div>
+                <div className="upload-section">
+                    <UploadItem/>
+                </div>
             </div>
         )
     }
--- imports/client/pages/clients/facilities/components/FacilityContent/UploadItem.jsx
+++ imports/client/pages/clients/facilities/components/FacilityContent/UploadItem.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-export default class Uploaditem extends Component {
+export default class UploadItem extends Component {
     render() {
 
         return (
