96fdce8006670ca52ea7855b917f8e103df38429
maxon1 <maxim.shmakov@cultofcoders.com>
2018-02-27 12:15:14 +0200

    updates

--- imports/api/clients/server/expose.js
+++ imports/api/clients/server/expose.js
@@ -1,6 +1,8 @@
 import Clients from './../collection';
 import ClientListQuery from './../queries/listClients';
+import ClientsWithFacilitiesQuery from './../queries/clientsWithFacilites';
 
 Clients.expose({});
 
 ClientListQuery.expose({});
+ClientsWithFacilitiesQuery.expose({});
