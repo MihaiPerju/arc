ee3a225b4f2d4bfc0926f39ef199272524d12704
MihaiPerju <mihai.perju@cultofcoders.com>
2018-02-28 23:23:55 +0200

    deleted semantic ui css and added uuid

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -4,7 +4,6 @@ import '/imports/client/lib/simpleSchemaMessages'
 import '/imports/api/users/collection'; // for helpers
 import svg4everybody from 'svg4everybody'; //external file svg polyfill
 svg4everybody();
-import 'semantic-ui-css/semantic.min.css';
 import '/imports/api/comments/links';
 import '/imports/api/facilities/links';
 import '/imports/api/tasks/links';
