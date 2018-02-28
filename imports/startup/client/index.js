fed0bf305b47ba78af26176571bcc60abe674ec1
MihaiPerju <mihai.perju@cultofcoders.com>
2018-02-28 16:54:27 +0200

    removed semantic

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,7 +3,6 @@ import '/imports/client/routing';
 import '/imports/client/lib/simpleSchemaMessages'
 import '/imports/api/users/collection'; // for helpers
 import svg4everybody from 'svg4everybody'; //external file svg polyfill
-import 'semantic-ui-css/semantic.min.css';
 import '/imports/api/comments/links';
 import '/imports/api/facilities/links';
 import '/imports/api/tasks/links';
