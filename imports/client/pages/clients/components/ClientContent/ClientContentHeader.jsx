8101f8e404721ac937681be5f0a24d58fde07c9c
maxon1 <maxim.shmakov@cultofcoders.com>
2018-03-03 12:37:44 +0200

    updates

--- imports/client/pages/clients/components/ClientContent/ClientContentHeader.jsx
+++ imports/client/pages/clients/components/ClientContent/ClientContentHeader.jsx
@@ -29,10 +29,7 @@ export default class ClientContentHeader extends Component {
                                className="cc-button btn--white">
                                 Manage facilities
                             </a>
-                            <a href={FlowRouter.url('region.list', {id: client._id})} className="cc-button btn--white">
-                                Manage regions
-                            </a>
-                            <button onClick={this.onRedirect.bind(this, client._id)} className="btn--white">Edit
+                            <button onClick={this.onEdit} className="btn--white">Edit
                                 client
                             </button>
                         </div>
