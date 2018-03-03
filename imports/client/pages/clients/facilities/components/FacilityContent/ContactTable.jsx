9e11ae74495ee218bba48e913c454a6399b0b211
maxon1 <maxim.shmakov@cultofcoders.com>
2018-03-03 12:03:50 +0200

    updates

---
+++ imports/client/pages/clients/facilities/components/FacilityContent/ContactTable.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class ContactTable extends Component {
+    render() {
+        return (
+            <div className="action-block">
+                <div className="header__block">
+                    <div className="title-block text-uppercase">Contacts</div>
+                </div>
+                <div className="main__block">
+
+                </div>
+            </div>
+        )
+    }
+}
