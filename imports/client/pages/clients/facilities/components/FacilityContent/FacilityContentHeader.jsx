4ea0a246dafbd98a91a482aa273fec78e3c88647
Alex Cojocariu <alexandru.cojocariu@cultofcoders.com>
2018-03-13 18:23:34 +0200

    fixed the FacilityContentHeader not showing the region correctly

--- imports/client/pages/clients/facilities/components/FacilityContent/FacilityContentHeader.jsx
+++ imports/client/pages/clients/facilities/components/FacilityContent/FacilityContentHeader.jsx
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import classNames from 'classnames';
+import RegionListQuery from '/imports/api/regions/queries/regionList.js';
 
 export default class FacilityContentHeader extends Component {
     constructor() {
         super();
         this.state = {
             dropdown: false
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps !== this.props){
+            if (this.props.facility.regionId){
+                RegionListQuery.clone({
+                    filters: {
+                        _id: this.props.facility.regionId
+                    }
+                }).fetchOne((err, region) => {
+                    if (!err){
+                        this.setState({
+                            region
+                        })
+                    }
+                })
+            } else {
+                this.setState({
+                    region: null
+                })
+            }
         }
     }
 
@@ -42,7 +65,7 @@ export default class FacilityContentHeader extends Component {
     }
 
     render() {
-        const {dropdown} = this.state;
+        const {dropdown, region} = this.state;
         const {facility} = this.props;
         const classes = classNames({
             'dropdown': true,
@@ -98,7 +121,7 @@ export default class FacilityContentHeader extends Component {
                     </li>
                     <li className="text-center">
                         <div className="text-light-grey">Region</div>
-                        <div className="info-label">None</div>
+                        <div className="info-label">{region ? region.name : "None"}</div>
                     </li>
                     <li className="text-center toggle-allow-users" onClick={this.openDropdown} ref={this.nodeRef}>
                         <div className={classes}>
