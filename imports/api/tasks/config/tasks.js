6fce89c45ed98b3f6960a017cfa72100bacaf206
Alex Cojocariu <alexandru.cojocariu@cultofcoders.com>
2018-04-13 05:35:00 +0300

    removed insCode2 and insCode3 from array

--- imports/api/tasks/config/tasks.js
+++ imports/api/tasks/config/tasks.js
@@ -3,6 +3,6 @@ export default {
     dates: ['dischrgDate', 'fbDate', 'admitDate'],
     numbers: ['medNo', 'insBal', 'insBal2', 'insBal3', 'acctBal'],
     links: ['facilityId', 'assigneeId'],
-    strings: ['acctNum', 'facCode', 'insCode', 'insCode2', 'insCode3', 'ptType', 'ptName',
+    strings: ['acctNum', 'facCode', 'insCode', 'ptType', 'ptName',
         'finClass', 'insName', 'insName2', 'insName3']
 }
