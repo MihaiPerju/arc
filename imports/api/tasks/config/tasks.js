120805c3b0570d8ef9e53840b92b6386896149ab
MihaiPerju <mihai.perju@cultofcoders.com>
2018-04-13 08:03:45 +0300

    filter report using client Id

--- imports/api/tasks/config/tasks.js
+++ imports/api/tasks/config/tasks.js
@@ -3,7 +3,7 @@ export default {
     dates: ['dischrgDate', 'fbDate', 'admitDate'],
     numbers: ['medNo', 'insCode', 'insCode2', 'insCode3',
         'insBal', 'insBal2', 'insBal3', 'acctBal'],
-    links: ['facilityId', 'assigneeId'],
+    links: ['facilityId', 'assigneeId', 'clientId'],
     strings: ['acctNum', 'facCode', 'ptType', 'ptName',
         'finClass', 'insName', 'insName2', 'insName3']
 }
