80295b667f6feff9de0f7289f29c049414004b70
Alex Cojocariu <alexandru.cojocariu@cultofcoders.com>
2018-04-13 03:50:16 +0300

    modified type of insCode

--- imports/api/tasks/config/tasks.js
+++ imports/api/tasks/config/tasks.js
@@ -1,9 +1,8 @@
 export default {
     enums: ['state', 'substate'],
     dates: ['dischrgDate', 'fbDate', 'admitDate'],
-    numbers: ['medNo', 'insCode', 'insCode2', 'insCode3',
-        'insBal', 'insBal2', 'insBal3', 'acctBal'],
+    numbers: ['medNo', 'insBal', 'insBal2', 'insBal3', 'acctBal'],
     links: ['facilityId', 'assigneeId'],
-    strings: ['acctNum', 'facCode', 'ptType', 'ptName',
+    strings: ['acctNum', 'facCode', 'insCode', 'insCode2', 'insCode3', 'ptType', 'ptName',
         'finClass', 'insName', 'insName2', 'insName3']
 }
