84eb4d5e53fd5419707be452c33c48215bc7ca10
MihaiPerju <mihai.perju@cultofcoders.com>
2018-04-13 21:13:04 +0300

    deleted unused method

--- imports/api/tasks/config/tasks.js
+++ imports/api/tasks/config/tasks.js
@@ -2,6 +2,6 @@ export default {
     enums: ['state', 'substate'],
     dates: ['dischrgDate', 'fbDate', 'admitDate'],
     numbers: ['medNo', 'insBal', 'insBal2', 'insBal3', 'acctBal'],
-    strings: ['acctNum', 'facCode', 'insCode', 'ptType', 'ptName', 'finClass', 'insName', 'insName2', 'insName3']
+    strings: ['acctNum', 'facCode', 'insCode', 'ptType', 'ptName', 'finClass', 'insName', 'insName2', 'insName3'],
     links: ['facilityId', 'assigneeId', 'clientId'],
 }
