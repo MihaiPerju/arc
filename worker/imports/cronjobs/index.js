c5da40ae9dcba2c5caa0b504d553df5726fc1458
MihaiPerju <mihai.perju@cultofcoders.com>
2018-05-09 10:05:45 +0300

    initial commit

--- worker/imports/cronjobs/index.js
+++ worker/imports/cronjobs/index.js
@@ -1,7 +1,21 @@
 import RunReports from "../services/RunReports";
 
+//Job for running reports
 SyncedCron.add({
-  name: "Scan ",
+  name: "Run Report",
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text("every 10 seconds");
+  },
+  job: function() {
+    RunReports.run();
+  }
+});
+
+
+//Job for sending letters
+SyncedCron.add({
+  name: "Send Letters",
   schedule: function(parser) {
     // parser is a later.parse object
     return parser.text("every 10 seconds");
