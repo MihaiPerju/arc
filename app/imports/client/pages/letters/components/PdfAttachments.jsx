89b6fa159ccc5811e2f6b7261dc3d1e16e52178c
MihaiPerju <mihai.perju@cultofcoders.com>
2018-05-14 18:16:29 +0300

    added pdf attachments file

---
+++ app/imports/client/pages/letters/components/PdfAttachments.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import TaskViewService from "/imports/client/pages/tasks/services/TaskViewService";
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove
+} from "react-sortable-hoc";
+
+export default class PdfAttachments extends Component {
+  render() {
+    const { attachments, onRemoveAttachment, onOrderChange } = this.props;
+    return (
+      <div className="attachment-list">
+        <SortableComponent
+          onRemoveAttachment={onRemoveAttachment}
+          onOrderChange={onOrderChange}
+          items={attachments}
+        />
+      </div>
+    );
+  }
+}
+
+const SortableItem = SortableElement(({ item, onRemoveAttachment }) => {
+  remove = () => {
+    onRemoveAttachment(item._id);
+  };
+
+  return (
+    <div className="attachment-item">
+      <span className="text-white truncate">{item.name}</span>
+      <div className="btn-rmv">
+        <a className="icon-trash-o" onClick={this.remove.bind(this)} />
+      </div>
+    </div>
+  );
+});
+
+const SortableList = SortableContainer(({ items, onRemoveAttachment }) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <SortableItem
+          onRemoveAttachment={onRemoveAttachment}
+          key={item._id}
+          index={index}
+          item={item}
+        />
+      ))}
+    </ul>
+  );
+});
+
+class SortableComponent extends Component {
+  state = {
+    items: this.props.items
+  };
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState({
+      items: arrayMove(this.state.items, oldIndex, newIndex)
+    });
+    this.props.onOrderChange(this.state.items);
+  };
+  render() {
+    return (
+      <SortableList
+        onRemoveAttachment={this.props.onRemoveAttachment}
+        items={this.state.items}
+        onSortEnd={this.onSortEnd}
+        distance={10}
+      />
+    );
+  }
+}
