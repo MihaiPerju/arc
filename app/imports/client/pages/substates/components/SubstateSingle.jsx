1cb0072d899d2539304ad660fca96553daf67cd9
Aditya G <aditya.g@cisinlabs.com>
2018-05-17 19:45:53 +0530

    - added the dynamic actions according to the substates

--- app/imports/client/pages/substates/components/SubstateSingle.jsx
+++ app/imports/client/pages/substates/components/SubstateSingle.jsx
@@ -1,73 +1,113 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import classNames from "classnames";
-import Notifier from '/imports/client/lib/Notifier';
+import Notifier from "/imports/client/lib/Notifier";
+import actionQuery from "/imports/api/actions/queries/actionList";
 
 export default class SubstateSingle extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      actions: []
+    };
+  }
 
-    onSetSubstate = () => {
-        const { substate, setSubstate } = this.props;
-        setSubstate(substate._id);
-    }
+  componentWillMount() {
+    const { substate } = this.props;
+    this.getActions(substate.actionIds);
+  }
 
-    onSelectSubstate = (e) => {
-        e.stopPropagation();
-        const { substate, selectSubstate } = this.props;
-        selectSubstate(substate._id);
-    }
+  componentWillReceiveProps(newProps) {
+    const { substate } = newProps;
+    this.getActions(substate.actionIds);
+  }
 
-    deleteSubstate = (_id) => {
-        Meteor.call('substate.delete', _id, (err, res) => {
-            if (!err) {
-                Notifier.success('Deleted Successfully !');
-            }
-        })
-    }
+  getActions = actionIds => {
+    actionQuery
+      .clone({
+        filters: {
+          _id: { $in: actionIds }
+        }
+      })
+      .fetch((err, actions) => {
+        if (!err) {
+          this.setState({ actions });
+        }
+      });
+  };
 
-    render() {
-        const { substate, substateSelected, currentSubstate } = this.props;
-        const checked = substateSelected.includes(substate._id);
-        const classes = classNames({
-            "table_row": true,
-            "right-side": true,
-            "bg--yellow": checked,
-            "open": currentSubstate === substate._id
-        });
+  onSetSubstate = () => {
+    const { substate, setSubstate } = this.props;
+    setSubstate(substate._id);
+  };
 
-        return (
-            <div className={classes}>
-                <div className='table-small'>
-                    <div className='table-cell'>
-                        <div className="check-item">
-                            <input checked={checked} type="checkbox" className="hidden" />
-                            <label onClick={this.onSelectSubstate}></label>
-                        </div>
-                    </div>
-                </div>
-                <div className='table-small'>
-                    <div className='table-cell'>{substate.stateName}</div>
-                </div>
-                <div className='table-small'>
-                    <div className='table-cell'>{substate.name}</div>
-                </div>
-                <div className='table-small'>
-                    <div className='table-cell'>{substate.description}</div>
-                </div>
-                <div className='table-small'>
-                    <div className='table-cell'>
-                        <a href="">Action 1</a>
-                        <a href="">Action 2</a>
-                    </div>
-                </div>
-                <div className='table-small'>
-                    <div className='table-cell'>
-                        <button onClick={this.onSetSubstate} className="btn-text--blue">edit</button>
-                        <button onClick={() => this.deleteSubstate(substate._id)} className="btn-text--red"><i className="icon-trash-o"></i></button>
-                    </div>
-                </div>
+  onSelectSubstate = e => {
+    e.stopPropagation();
+    const { substate, selectSubstate } = this.props;
+    selectSubstate(substate._id);
+  };
+
+  deleteSubstate = _id => {
+    Meteor.call("substate.delete", _id, (err, res) => {
+      if (!err) {
+        Notifier.success("Deleted Successfully !");
+      }
+    });
+  };
+
+  render() {
+    const { substate, substateSelected, currentSubstate } = this.props;
+    const { actions } = this.state;
+    const checked = substateSelected.includes(substate._id);
+    const classes = classNames({
+      table_row: true,
+      "right-side": true,
+      "bg--yellow": checked,
+      open: currentSubstate === substate._id
+    });
+
+    return (
+      <div className={classes}>
+        <div className="table-small">
+          <div className="table-cell">
+            <div className="check-item">
+              <input checked={checked} type="checkbox" className="hidden" />
+              <label onClick={this.onSelectSubstate} />
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className="table-small">
+          <div className="table-cell">{substate.stateName}</div>
+        </div>
+        <div className="table-small">
+          <div className="table-cell">{substate.name}</div>
+        </div>
+        <div className="table-small">
+          <div className="table-cell">{substate.description}</div>
+        </div>
+        <div className="table-small">
+          <div className="table-cell">
+            {actions.map(action => (
+              // @TODO: maintain the href
+              <a className="text-blue" href={action._id}>
+                {action.title}
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="table-small">
+          <div className="table-cell">
+            <button onClick={this.onSetSubstate} className="btn-text--blue">
+              edit
+            </button>
+            <button
+              onClick={() => this.deleteSubstate(substate._id)}
+              className="btn-text--red"
+            >
+              <i className="icon-trash-o" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
