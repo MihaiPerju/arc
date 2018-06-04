9e0f9853930ac511aa0b99768af801f78f19244e
maxon1 <maxim.shmakov@cultofcoders.com>
2018-06-04 20:29:46 +0300

    [TASK-310] - fixed mgmt page

--- app/imports/client/pages/tags/components/TagContent/TagContentDescription.jsx
+++ app/imports/client/pages/tags/components/TagContent/TagContentDescription.jsx
@@ -1,88 +1,88 @@
-import React, { Component } from 'react';
-import { AutoForm, SelectField, ErrorField } from '/imports/ui/forms';
+import React, {Component} from 'react';
+import {AutoForm, SelectField, ErrorField} from '/imports/ui/forms';
 import SimpleSchema from 'simpl-schema';
 import TagContentSingle from './TagContentSingle';
 import Notifier from "/imports/client/lib/Notifier";
+import SelectMulti from "/imports/client/lib/uniforms/SelectMulti.jsx";
+import ReportsEnum from "../../../../../api/schedules/enums/reports";
 
 export default class TagContentDescription extends Component {
 
-	getOptions = (users) => {
-		return _.map(users, (user) => ({
-			value: user._id,
-			label: `${user.profile.firstName} ${user.profile.lastName}`
-		}));
-	};
+    getOptions = (users) => {
+        return _.map(users, (user) => ({
+            value: user._id,
+            label: `${user.profile.firstName} ${user.profile.lastName}`
+        }));
+    };
 
-	onSubmit = (data) => {
-		const { userId } = data;
-		const { currentTag } = this.props;
-		Meteor.call('user.addTag', { _id: userId, tagId: currentTag._id }, (err, res) => {
-			if (!err) {
-				Notifier.success('Successfully added !')
-			}
-		})
-	}
+    onSubmit = (data) => {
+        const {userId} = data;
+        const {currentTag} = this.props;
+        Meteor.call('user.addTag', {_id: userId, tagId: currentTag._id}, (err, res) => {
+            if (!err) {
+                Notifier.success('Successfully added !')
+            }
+        })
+    }
 
-	render() {
-		const { users, currentTag, untaggedUsers, taggedUsers } = this.props;
-		const options = this.getOptions(untaggedUsers);
+    render() {
+        const {users, currentTag, untaggedUsers, taggedUsers} = this.props;
+        const options = this.getOptions(untaggedUsers);
 
-		return (
-			<div className="action-block">
-				<div className="header__block">
-					<div className="title-block text-uppercase">Users</div>
-				</div>
+        return (
+            <div className="create-form">
+                <div className="action-block i--block">
+                    <div className="header__block">
+                        <div className="title-block text-uppercase">Users</div>
+                    </div>
+                    <AutoForm schema={schema} onSubmit={this.onSubmit.bind(this)} ref="form">
+                        <div className="select-group">
+                            <div className="form-wrapper">
+                                <SelectMulti className="form-select__multi"
+                                             placeholder="Select User"
+                                             labelHidden={true}
+                                             name="userId"
+                                             options={options}/>
+                                <ErrorField name="userId"/>
+                            </div>
+                        </div>
+                        <div className="btn-group-1 flex--helper">
+                            <button className="btn--red">Remove all</button>
+                            <button className="btn--green">Submit</button>
+                        </div>
+                    </AutoForm>
+                </div>
 
-				<div className="create-form">
-					<div className="create-form__wrapper">
-						<div className="action-block">
-							<AutoForm schema={schema} onSubmit={this.onSubmit.bind(this)} ref="form">
-								<div className="select-group">
-									<div className="form-wrapper">
-										<SelectField placeholder="Select User"
-											labelHidden={true}
-											options={options}
-											name="userId" />
-										<ErrorField name="userId" />
-									</div>
-								</div>
-								<button style={{ float: 'right' }} className="btn--green">submit</button>
-							</AutoForm>
-						</div>
-					</div>
-				</div>
-				<div className="table-list">
-					<div className="table-list__wrapper">
-						<div className="table-container">
-							<div className="table-row">
-								<div className="table-header text-center table-field text-light-grey">
-									Name
-                                </div>
-								<div className="table-header text-center table-field text-light-grey">
-									Actions
-                                </div>
-							</div>
-							{
-								taggedUsers.map((user, index) => (
-									<TagContentSingle
-										key={index}
-										userName={`${user.profile.firstName} ${user.profile.lastName}`}
-										userId={user._id}
-										currentTag={currentTag}
-									/>
-								))
-							}
-						</div>
-					</div>
-				</div>
+                <div className="action-table">
+                    <div className="action-table__wrapper">
+                        <div className="action-table__row flex--helper">
+                            <div className="action-table__header action-table__field text-light-grey">
+                                Name
+                            </div>
+                            <div className="action-table__header action-table__field text-center text-light-grey">
+                                Actions
+                            </div>
+                        </div>
+                        {
+                            taggedUsers.map((user, index) => (
+                                <TagContentSingle
+                                    key={index}
+                                    userName={`${user.profile.firstName} ${user.profile.lastName}`}
+                                    userId={user._id}
+                                    currentTag={currentTag}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
 
-			</div>
-		)
-	}
+        )
+    }
 }
 
 const schema = new SimpleSchema({
-	userId: {
-		type: String
-	}
+    userId: {
+        type: String
+    }
 });
