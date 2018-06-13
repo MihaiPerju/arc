ad81e2b61c3ed8ca55b0cdda173cf3e37a621a17
Michael Dawson <mihai.perju@cultofcoders.com>
2018-06-13 17:54:04 +0300

    added missing files

---
+++ app/imports/api/notifications/queries/notificationList.js
@@ -0,0 +1,13 @@
+import Notifications from "../collection";
+
+export default Notifications.createNamedQuery("notificationList", {
+  $filter({ filters, options, params }) {
+    _.extend(filters, params.filters);
+    _.extend(options, params.options);
+  },
+  $paginate: true,
+  seen: 1,
+  receiverId: 1,
+  message: 1,
+  type: 1
+});
