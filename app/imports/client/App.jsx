786062e96cf13a8aa9e1325e629c9fe8c3efc507
maxon1 <maxim.shmakov@cultofcoders.com>
2018-06-14 17:13:18 +0300

    fixed styles

--- app/imports/client/App.jsx
+++ app/imports/client/App.jsx
@@ -3,7 +3,6 @@ import NotificationsSystem from 'react-notification-system';
 import Notifier from '/imports/client/lib/Notifier';
 import Header from '/imports/client/layout/header/Header';
 import LeftMenu from '/imports/client/layout/leftMenu/LeftMenu';
-import Footer from '/imports/client/layout/footer/Footer';
 import Login from "/imports/client/pages/users/Login";
 
 export default ({main, routeProps}) => {
