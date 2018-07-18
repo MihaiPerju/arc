ed6f3cc6ac712601cee0390d3990f5d6584987eb
onkar <onkar.g@cultofcoders.com>
2018-07-18 15:49:10 +0530

    ARCC-1-add-qr-to-all-outgoing-mail user can see QR code on click of download and view icon  --3hr

--- app/imports/api/letters/server/letter.service.js
+++ app/imports/api/letters/server/letter.service.js
@@ -1,28 +1,29 @@
-import Letters from '../collection.js';
-import htmlToPdf from 'html-to-pdf';
-import os from 'os';
-import Future from 'fibers/future';
+import Letters from "../collection.js";
+import pdf from "html-pdf";
+import os from "os";
+import Future from "fibers/future";
 
 class LetterService {
-    getLetterTemporalPdfLoc(accountId, letterId) {
-        const letter = Letters.findOne({
-            _id: letterId,
-            accountId,
-        }, {
-            fields: {body: 1},
-        });
+  getLetterTemporalPdfLoc(accountId, letterId) {
+    const letter = Letters.findOne(
+      {
+        _id: letterId,
+        accountId
+      },
+      {
+        fields: { body: 1 }
+      }
+    );
+    const fileLocation = `${os.tmpdir()}/${letter._id}.pdf`;
+    let future = new Future();
 
-        const fileLocation = `${os.tmpdir()}/${letter._id}.pdf`;
-        let future = new Future();
+    pdf.create(letter.body).toFile(fileLocation, function(error, success) {
+      future.return(error ? { error } : { success });
+    });
 
-        htmlToPdf.convertHTMLString(letter.body, fileLocation,
-            function(error, success) {
-                future.return(error ? {error} : {success});
-            },
-        );
-        const {success} = future.wait();
-        return success ? fileLocation : success;
-    }
+    const { success } = future.wait();
+    return success ? fileLocation : success;
+  }
 }
 
 export default new LetterService();
