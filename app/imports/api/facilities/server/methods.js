6780db597d46bd72acd320b62a9c81e6f2c9b777
MihaiPerju <mihaiperju98@gmail.com>
2018-07-28 14:30:13 +0300

    fixed facility logos

--- app/imports/api/facilities/server/methods.js
+++ app/imports/api/facilities/server/methods.js
@@ -1,4 +1,6 @@
-import { Meteor } from "meteor/meteor";
+import {
+  Meteor
+} from "meteor/meteor";
 import Facilities from "/imports/api/facilities/collection.js";
 import Security from "/imports/api/security/security.js";
 import FacilitySchema from "../schema.js";
@@ -7,43 +9,47 @@ import fs from "fs";
 import os from "os";
 import Business from "/imports/api/business";
 import Uploads from "../../s3-uploads/uploads/collection";
+import Settings from "/imports/api/settings/collection.js";
 
 Meteor.methods({
-  "facility.create"(data) {
+  "facility.create" (data) {
     Security.isAdminOrTech(this.userId);
 
     Facilities.insert(data);
   },
 
-  "facility.get"(facilityId) {
+  "facility.get" (facilityId) {
     Security.isAdminOrTech(this.userId);
     return Facilities.findOne(facilityId);
   },
 
-  "facility.update"(facility) {
+  "facility.update" (facility) {
     Security.isAdminOrTech(this.userId);
     const facilityData = FacilitySchema.clean(facility);
-    Facilities.update(
-      { _id: facility._id },
-      {
-        $set: facilityData
-      }
-    );
+    Facilities.update({
+      _id: facility._id
+    }, {
+      $set: facilityData
+    });
   },
 
-  "facility.remove"(facilityId) {
+  "facility.remove" (facilityId) {
     Security.isAdminOrTech(this.userId);
 
     Facilities.remove(facilityId);
   },
 
-  "facility.removeMany"(facilityIds) {
+  "facility.removeMany" (facilityIds) {
     Security.isAdminOrTech(this.userId);
 
-    Facilities.remove({ _id: { $in: facilityIds } });
+    Facilities.remove({
+      _id: {
+        $in: facilityIds
+      }
+    });
   },
 
-  "facility.getRegions"(regionIds) {
+  "facility.getRegions" (regionIds) {
     Security.isAdminOrTech(this.userId);
 
     return Regions.find({
@@ -53,39 +59,49 @@ Meteor.methods({
     }).fetch();
   },
 
-  "facility.getNames"() {
-    return Facilities.find({}, { name: 1 }).fetch();
+  "facility.getNames" () {
+    return Facilities.find({}, {
+      name: 1
+    }).fetch();
   },
 
-  "facility.getLogo"(_id) {
-    const facility = Facilities.findOne({ _id });
-    const { logoPath } = facility;
-    return logoPath;
-  },
-
-  "facility.removeLogo"(_id, path) {
-    Facilities.update(
-      { _id },
-      {
-        $unset: {
-          logoPath: null
-        }
+  "facility.removeLogo" (_id) {
+    const {
+      rootFolder
+    } = Settings.findOne({
+      rootFolder: {
+        $ne: null
       }
-    );
-    fs.unlinkSync(Business.LOCAL_STORAGE_FOLDER + "/" + path);
-    Uploads.remove({ path });
+    });
+
+    const {
+      logoPath
+    } = Facilities.findOne({
+      _id
+    });
+
+    Facilities.update({
+      _id
+    }, {
+      $unset: {
+        logoPath: null
+      }
+    });
+    fs.unlinkSync(rootFolder + Business.CLIENTS_FOLDER + logoPath);
+    Uploads.remove({
+      path: logoPath
+    });
   },
 
-  "facility.switchStatus"(_id, status) {
+  "facility.switchStatus" (_id, status) {
     Security.isAdminOrTech(this.userId);
 
-    return Facilities.update(
-      { _id },
-      {
-        $set: {
-          status: !status
-        }
+    return Facilities.update({
+      _id
+    }, {
+      $set: {
+        status: !status
       }
-    );
+    });
   }
 });
