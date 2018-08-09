0179b7550c8ceca85395f7a781dbe89cb240ba26
maxon1 <maxim.shmakov@cultofcoders.com>
2018-08-09 18:31:02 +0300

    style setting page

--- app/imports/client/pages/admin/Settings.jsx
+++ app/imports/client/pages/admin/Settings.jsx
@@ -1,58 +1,63 @@
-import React from "react";
-import { AutoForm, AutoField, ErrorField } from "/imports/ui/forms";
-import SimpleSchema from "simpl-schema";
-import Notifier from "/imports/client/lib/Notifier";
+import React from 'react';
+import {AutoForm, AutoField, ErrorField} from '/imports/ui/forms';
+import SimpleSchema from 'simpl-schema';
+import Notifier from '/imports/client/lib/Notifier';
 
 export default class Settings extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super ();
     this.state = {
-      model: {}
+      model: {},
     };
   }
 
-  componentWillMount() {
-    Meteor.call("admin.getRootFolder", (err, model) => {
+  componentWillMount () {
+    Meteor.call ('admin.getRootFolder', (err, model) => {
       if (!err) {
-        this.setState({ model });
+        this.setState ({model});
       } else {
-        Notifier.error(err.reason);
+        Notifier.error (err.reason);
       }
     });
   }
 
-  onSubmit(data) {
-    Meteor.call("admin.updateRootFolder", data, err => {
+  onSubmit (data) {
+    Meteor.call ('admin.updateRootFolder', data, err => {
       if (!err) {
-        Notifier.success("Settings updated!");
+        Notifier.success ('Settings updated!');
       } else {
-        Notifier.error(err.reason);
+        Notifier.error (err.reason);
       }
     });
   }
 
-  render() {
-    const { model } = this.state;
+  render () {
+    const {model} = this.state;
     return (
-      <div>
-        Admin Settings
-        <AutoForm model={model} onSubmit={this.onSubmit} schema={schema}>
-          <AutoField
-            labelHidden={true}
-            name="rootFolder"
-            placeholder="Type Root Directory"
-          />
-          <ErrorField name="rootFolder" />
-          <button>Save</button>
-        </AutoForm>
+      <div className="settings-container flex--helper flex-align--center flex-justify--center">
+        <div className="settings-container__wrapper text-center">
+          <i className="icon-cog" />
+          <h2>Admin Settings</h2>
+          <AutoForm className="settings-form" model={model} onSubmit={this.onSubmit} schema={schema}>
+            <AutoField
+              labelHidden={true}
+              name="rootFolder"
+              placeholder="Type Root Directory"
+            />
+            <ErrorField name="rootFolder" />
+            <div className="btn-group">
+              <button className="btn--green">Save</button>
+            </div>
+          </AutoForm>
+        </div>
       </div>
     );
   }
 }
 
-const schema = new SimpleSchema({
+const schema = new SimpleSchema ({
   rootFolder: {
     type: String,
-    optional: true
-  }
+    optional: true,
+  },
 });
