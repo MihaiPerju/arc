ddb5aa86d14e0caedcabb9903d39c40693396ebb
MihaiPerju <mihaiperju98@gmail.com>
2018-08-29 18:51:17 +0300

    changed string to enum key

--- app/imports/client/lib/PagerService.js
+++ app/imports/client/lib/PagerService.js
@@ -1,6 +1,7 @@
 import moment from "moment";
 import stateEnum from "/imports/api/accounts/enums/states";
 import RolesEnum, { roleGroups } from "/imports/api/users/enums/roles";
+import statuses from "/imports/api/files/enums/statuses";
 
 export default class PagerService {
   queryParams;
@@ -623,7 +624,7 @@ export default class PagerService {
       });
     } else {
       _.extend(params.filters, {
-        status: { $ne: "dismiss" }
+        status: { $ne: statuses.DISMISS }
       });
     }
   }
