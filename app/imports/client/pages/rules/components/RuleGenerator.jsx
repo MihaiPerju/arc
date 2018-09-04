a33b21a0a665e1a4c0f92b020a90b6a7e1aa8170
MihaiPerju <mihaiperju98@gmail.com>
2018-09-04 12:38:21 +0300

    integrated query builder

--- app/imports/client/pages/rules/components/RuleGenerator.jsx
+++ app/imports/client/pages/rules/components/RuleGenerator.jsx
@@ -1,67 +1,36 @@
 import React, { Component } from "react";
-import connectField from "uniforms/connectField";
+import TwoWayQuerybuilder from "react-two-way-querybuilder";
 
-class RuleGenerator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      rule: []
-    };
+const fields = [
+  {
+    name: "firstName",
+    operators: "all",
+    label: "First Name",
+    input: { type: "text" }
+  },
+  {
+    name: "lastName",
+    operators: "all",
+    label: "Last Name",
+    input: { type: "text" }
+  },
+  { name: "age", operators: "all", label: "Age", input: { type: "text" } },
+  {
+    name: "birthDate",
+    operators: "all",
+    label: "Birth date",
+    input: { type: "text" }
+  }
+];
+
+class App extends Component {
+  handleChange(event) {
+    console.log("query", event.query);
   }
 
-  onChange = () => {};
-
-  onAddStatement = () => {
-    let { rule } = this.state;
-    rule.push({ type: "single" });
-    this.setState({ rule });
-  };
-
-  onAddOrGroup = () => {
-    let { rule } = this.state;
-    rule.push({ type: "or" });
-    this.setState({ rule });
-  };
-
-  onAddAndGroup = () => {
-    let { rule } = this.state;
-    rule.push({ type: "and" });
-    this.setState({ rule });
-  };
-
   render() {
-    const { rule } = this.state;
-    return (
-      <div>
-        {rule.map((condition, index) => {
-          if (condition.type === "and") {
-            return (
-              <div>
-                <input label="Select action to fire" />
-              </div>
-            );
-          } else if (condition.type === "or") {
-            return (
-              <div>
-                <input label="Select action to fire" />
-              </div>
-            );
-          }
-          return (
-            <div>
-              <input label="Select action to fire" />
-            </div>
-          );
-        })}
-        <button onClick={this.onAddCondition}>Add Statement</button>
-        <button onClick={this.onAddOrGroup}>Add OR Group</button>
-        <button onClick={this.onAddAndGroup}>Add AND Group</button>
-      </div>
-    );
+    return <TwoWayQuerybuilder fields={fields} onChange={this.handleChange} />;
   }
 }
 
-export default connectField(RuleGenerator, {
-  ensureValue: false,
-  includeInChain: false
-});
+export default App;
