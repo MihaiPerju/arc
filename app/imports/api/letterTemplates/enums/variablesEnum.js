f0f144e4c3d98c01a4094c88073e5257106f6f53
walkwel-tech <developer.walkwel@gmail.com>
2018-09-11 14:17:15 +0530

    Add additional options to letter templates editor

--- app/imports/api/letterTemplates/enums/variablesEnum.js
+++ app/imports/api/letterTemplates/enums/variablesEnum.js
@@ -1,6 +1,11 @@
 export default [
     {label: "Account Number", value: "Account Number"},
     {label: "Facility Code", value: "Facility Code",},
+    {label: "Facility Name", value: "Facility Name",},
+    {label: "Facility Address", value: "Facility Address",},
+    {label: "Facility Address2", value: "Facility Address2",},
+    {label: "Facility City", value: "Facility City",},
+    {label: "Client Name", value: "Client Name",},
     {label: "Patient Type", value: "Patient Type",},
     {label: "Patient Name", value: "Patient Name",},
     {label: "Discharge Date", value: "Discharge Date",},
@@ -9,17 +14,39 @@ export default [
     {label: "Financial Class", value: "Financial Class",},
     {label: "Admit Date", value: "Admit Date",},
     {label: "Medical Number", value: "Medical Number",},
-    {label: "State", value: "State",},
+    {label: "StateZip", value: "StateZip",},
     {label: "Substate", value: "Substate",},
     {label: "User Last Name", value: "User Last Name",},
     {label: "User First Name", value: "User First Name",},
+    {label: " User Phone", value: " User Phone",},
+   
 ]
 export const variablesEnum = {
     "Account Number": {
         field: "acctNum",
         scope: "account"
     },
-    "Facility Code": {
+    "Facility Name": {
+        field: "facName",
+        scope: "account"
+    },
+    "Facility Address": {
+        field: "facAddress",
+        scope: "account"
+    },
+    "Facility Address 2": {
+        field: "facAddress2",
+        scope: "account"
+    },
+    "Facility City": {
+        field: "facCity",
+        scope: "account"
+    },
+    "Client Name": {
+        field: "clientName",
+        scope: "account"
+    },
+     "Facility Code": {
         field: "facCode",
         scope: "account"
     },
@@ -54,8 +81,8 @@ export const variablesEnum = {
         field: "medNo",
         scope: "account"
     },
-    "State": {
-        field: "state",
+    "StateZip": {
+        field: "StateZip",
         scope: "account"
     },
     "Substate": {
@@ -68,6 +95,10 @@ export const variablesEnum = {
     },
     "User First Name": {
         field: "firstName",
+        scope: "user"
+    },
+    "User Phone": {
+        field: "phone",
         scope: "user"
     },
 };
