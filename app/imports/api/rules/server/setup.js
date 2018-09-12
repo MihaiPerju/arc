a26028f1cec8d588ed40f283755469ab1abdc26b
MihaiPerju <mihaiperju98@gmail.com>
2018-09-12 22:00:12 +0300

    index ready

--- app/imports/api/rules/server/setup.js
+++ app/imports/api/rules/server/setup.js
@@ -3,9 +3,8 @@ import Rules from "/imports/api/rules/collection";
 Meteor.startup(function() {
   const RulesNative = Rules.rawCollection();
   RulesNative.createIndex({ clientId: 1, facilityId: 1, priority: 1 });
-
-  //   const changeStream = RulesNative.watch();
-  //   changeStream.on("change", data => {
-  // console.log(data);
-  //   });
+  // const changeStream = RulesNative.watch();
+  // changeStream.on("change", data => {
+  //     console.log(data);
+  // });
 });
