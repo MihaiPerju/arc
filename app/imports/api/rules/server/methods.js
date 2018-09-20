69c1449b4b9bff63dec59f4e500df31a8cbf6e83
MihaiPerju <mihaiperju98@gmail.com>
2018-09-20 14:23:27 +0300

    parsing dates save

--- app/imports/api/rules/server/methods.js
+++ app/imports/api/rules/server/methods.js
@@ -3,44 +3,43 @@ import triggerTypes, {triggerOptions} from '/imports/api/rules/enums/triggers';
 
 Meteor.methods ({
   'rule.create' (data) {
-    console.log (data);
-    //   const {priority, clientId} = data;
-    //   Rules.update (
-    //     {
-    //       priority: {$gte: priority},
-    //       clientId,
-    //     },
-    //     {
-    //       $inc: {priority: 1},
-    //     },
-    //     {multi: true}
-    //   );
-    //   Rules.insert (data);
-    // },
+    const {priority, clientId} = data;
+    Rules.update (
+      {
+        priority: {$gte: priority},
+        clientId,
+      },
+      {
+        $inc: {priority: 1},
+      },
+      {multi: true}
+    );
+    Rules.insert (data);
+  },
 
-    // 'rule.update' (data) {
-    //   const {priority, clientId} = data;
-    //   //Increase priority for all the rules that have a priority greater than or equal to the new one
-    //   Rules.update (
-    //     {
-    //       priority: {$gte: priority},
-    //       clientId,
-    //     },
-    //     {
-    //       $inc: {priority: 1},
-    //     },
-    //     {multi: true}
-    //   );
+  'rule.update' (data) {
+    const {priority, clientId} = data;
+    //Increase priority for all the rules that have a priority greater than or equal to the new one
+    Rules.update (
+      {
+        priority: {$gte: priority},
+        clientId,
+      },
+      {
+        $inc: {priority: 1},
+      },
+      {multi: true}
+    );
 
-    //   //Update the rule itself
-    //   Rules.update (
-    //     {
-    //       _id: data._id,
-    //     },
-    //     {
-    //       $set: data,
-    //     }
-    //   );
+    //Update the rule itself
+    Rules.update (
+      {
+        _id: data._id,
+      },
+      {
+        $set: data,
+      }
+    );
   },
   'rule.delete' (_id) {
     //Take care of the security
--- app/imports/client/pages/rules/RuleCreate.jsx
+++ app/imports/client/pages/rules/RuleCreate.jsx
@@ -144,13 +144,13 @@ export default class RuleCreate extends React.Component {
 
   onSubmit = data => {
     console.log (data);
-    Meteor.call ('rule.create', data, (err, res) => {
-      if (!err) {
-        Notifier.success ('Rule added!');
-      } else {
-        Notifier.error (err.reason);
-      }
-    });
+    // Meteor.call ('rule.create', data, (err, res) => {
+    //   if (!err) {
+    //     Notifier.success ('Rule added!');
+    //   } else {
+    //     Notifier.error (err.reason);
+    //   }
+    // });
   };
 
   onCreateRule = () => {
@@ -173,7 +173,6 @@ export default class RuleCreate extends React.Component {
       workQueueOptions,
       actionOptions,
     } = this.state;
-    console.log (moment ());
     return (
       <div className="create-form">
         <div className="create-form__bar">
