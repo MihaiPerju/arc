865493b685279c77ad30a4f38599a966bbe9eab8
MihaiPerju <mihaiperju98@gmail.com>
2018-10-17 14:43:27 +0100

    added error handling

--- app/imports/api/accounts/server/accountPdfUpload.js
+++ app/imports/api/accounts/server/accountPdfUpload.js
@@ -1,5 +1,7 @@
 import Accounts from "../collection";
-import { createRoute } from "/imports/api/uploads/server/router";
+import {
+  createRoute
+} from "/imports/api/uploads/server/router";
 import Uploads from "/imports/api/uploads/uploads/collection";
 import Business from "/imports/api/business";
 import fs from "fs";
@@ -8,7 +10,13 @@ import settings from "/imports/api/settings/enums/settings";
 
 createRoute(
   "/uploads/account-pdf/:accountId/:token",
-  ({ user, accountId, error, filenames, success, uploadLocal }) => {
+  ({
+    user,
+    accountId,
+    filenames,
+    success,
+    uploadLocal
+  }) => {
     if (!user) {
       return error("Not logged in!");
     }
@@ -17,21 +25,32 @@ createRoute(
       return error("Invalid number of files");
     }
 
-    const { root } = SettingsService.getSettings(settings.ROOT);
+    const rootSettings = SettingsService.getSettings(settings.ROOT);
+    const root = rootSettings && rootSettings.root;
+    console.log(root);
+    if (!root) {
+      const [uploadId] = uploadLocal({
+        accountId
+      });
+      const {
+        path
+      } = Uploads.findOne({
+        _id: uploadId
+      });
+      const movePath = root + Business.ACCOUNTS_FOLDER + accountId + "/" + path;
+      fs.renameSync(root + path, movePath);
 
-    const [uploadId] = uploadLocal({ accountId });
-    const { path } = Uploads.findOne({ _id: uploadId });
-    const movePath = root + Business.ACCOUNTS_FOLDER + accountId + "/" + path;
-    fs.renameSync(root + path, movePath);
-
-    Accounts.update(
-      { _id: accountId },
-      {
+      Accounts.update({
+        _id: accountId
+      }, {
         $push: {
           attachmentIds: uploadId
         }
-      }
-    );
-    success();
+      });
+      success();
+    } else {
+      throw new Meteor.error("Upload failed");
+    }
+
   }
 );
