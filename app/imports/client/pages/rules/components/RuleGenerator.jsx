ae86b1f4f685531c74a504935866d9ef0396a396
MihaiPerju <mihaiperju98@gmail.com>
2018-10-17 02:21:48 +0100

    finished

--- app/imports/client/pages/rules/components/RuleGenerator.jsx
+++ app/imports/client/pages/rules/components/RuleGenerator.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import TwoWayQuerybuilder from "./rule-generator/TwoWayQuerybuilder";
 import connectField from "uniforms/connectField";
-import fields from "./rule-generator/helpers/fields";
+import Notifier from "/imports/client/lib/Notifier";
+import Loading from "/imports/client/lib/ui/Loading";
 
 class RuleGenerator extends Component {
   constructor() {
     super();
+    this.state = {
+      fields: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    Meteor.call("rules.getFields", (err, fields) => {
+      if (!err) {
+        this.setState({ isLoading: false, fields });
+      } else {
+        Notifier.error(err.reason);
+      }
+    });
   }
 
   onChange = query => {
@@ -15,7 +30,12 @@ class RuleGenerator extends Component {
   render() {
     const query = this.props.value && this.props.value.query;
     const config = { query };
+    const { fields, isLoading } = this.state;
+    console.log(fields);
 
+    if (isLoading) {
+      return <Loading />;
+    }
     return (
       <TwoWayQuerybuilder
         config={config}
