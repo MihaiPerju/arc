434f74fff05a5b50fbc8947f132045f2a8ca8469
INTELLECTYX iMAC <pskumar@intellectyx.com>
2018-10-31 23:11:40 +0530

    ARCC-282 In progress

--- app/imports/client/pages/clients/facilities/components/FacilityContent/PlacementBlock.jsx
+++ app/imports/client/pages/clients/facilities/components/FacilityContent/PlacementBlock.jsx
@@ -3,10 +3,25 @@ import Notifier from "../../../../../lib/Notifier";
 import DropzoneComponent from 'react-dropzone-component';
 import ImportingRules from '../ImportingRules';
 import { getToken } from "/imports/api/uploads/utils";
+import DatePicker from "react-datepicker";
+import moment from "moment";
 
 export default class PlacementBlock extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+          placementDate: moment()
+        };
+      }
+
+    onDateSelect = selectedDate => {
+        this.setState({ placementDate: selectedDate });
+      };
+
     render() {
         const {facility, setTempRules} = this.props;
+        const {placementDate} = this.state;
         const componentConfig = {
             postUrl: `/uploads/csv/${facility._id}/${getToken()}`
         };
@@ -24,6 +39,23 @@ export default class PlacementBlock extends Component {
                 <div className="header__block">
                     <div className="title-block text-uppercase">Placement file</div>
                 </div>
+                <div className="upload-section">
+                    <div className="radio-group flex--helper flex-align--center">
+                        <label>Account Placement Date:</label>
+                        <DatePicker
+                        calendarClassName="cc-datepicker"
+                        showMonthDropdown
+                        showYearDropdown
+                        todayButton={"Today"}
+                        placeholderText="Account Placement Date"
+                        selected={placementDate}
+                        name="placementDate"
+                        onChange={date =>
+                            this.onDateSelect(date, "placementDate")
+                        }
+                        />
+                    </div>
+                  </div>
                 <div className="main__block">
                     <div className="add-content">
                         <i className="icon-upload"/>
