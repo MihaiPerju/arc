6bdb3b87214b19a1ab15f7baf0f3643715867320
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2018-11-12 16:49:23 -0600

    Add styles to CRA and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import Header from './componenets/header/Header';
+import './styles/cc-app.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        
+        this.state = {
+            loggedIn: false,
+            user: {
+                role: 'Admin',
+                firstName: 'Ezekiel',
+                lastName: 'Keator'
+            }
+        };
+    }
+    
+    render() {
+        return (
+            <div id="cc-app">
+                <Header user={this.state.user} loggedIn={this.state.loggedIn}/>
+            </div>
+        );
+    }
 }
-
+    
 export default App;
