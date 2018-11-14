0919087d18e0706efd836c1b018c2fe21a6096d2
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2018-11-14 16:44:45 -0600

    WIP

---
+++ client/src/components/accounts/AccountList.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import TopBar from '../TopBar';
+
+export default class AccountList extends React.PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            accountsSelected: [],
+            currentAccount: null,
+            page: 1,
+            perPage: 13,
+            total: 0,
+            showMetaData: false,
+            openPane: false
+        };
+    }
+    
+    componentWillMount() {
+        // TODO: Fetch users accounts
+    }
+    
+    componentWillReceiveProps(newProps) {
+        // TODO: Clear filtering if remounted
+    }
+    
+    componentWillUnmount() {
+        // TODO: Remove any open locks
+        // this.removeLock();
+    }
+    
+    // selectAccount = newAccount => {
+    //     const { currentAccount } = this.state;
+    //     this.removeLock();
+    //     // removing accountId from the query when navigating from notification
+    //     FlowRouter.setQueryParams({ accountId: null });
+    //     if (this.checkAccountIsLocked(newAccount)) {
+    //         if (currentAccount === newAccount._id) {
+    //             this.closeRightPanel();
+    //         } else {
+    //             this.setState({
+    //                 currentAccount: newAccount._id,
+    //                 showMetaData: false
+    //             });
+    //             const { state } = FlowRouter.current().params;
+    //             if (state === "active") {
+    //                 this.incrementViewCount(newAccount._id);
+    //             }
+    //             this.addLock(newAccount._id);
+    //         }
+    //     }
+    // };
+    
+    incrementViewCount = _id => {
+        // TODO: Track the user viewed the acct
+    };
+    
+    checkAllAccount = selectAll => {
+        // TODO: Allow a user to select all accounts matching filters
+    };
+    
+    changeFilters(filters) {
+        this.updateFilters({ filters });
+    }
+    
+    assignToUser = () => {
+        // TODO: Assign users to accounts
+    };
+    
+    assignToWorkQueue = () => {
+        // TODO: Assign to workQueue (user tags)
+    };
+    
+    nextPage = inc => {
+        // TODO: Fetch next page
+    };
+    
+    openMetaDataSlider = () => {
+        this.setState({showMetaData: true});
+    };
+    
+    closeMetaDataSlider = () => {
+        this.setState({showMetaData: false});
+    };
+    
+    closeRightPanel = () => {
+        this.setState({ currentAccount: null, showMetaData: false });
+    };
+    
+    addLock = _id => {
+        // TODO: Lock an account
+    };
+    
+    removeLock = () => {
+        // TODO: Unlock and account
+    };
+    
+    render() {
+        if (this.props.isLoading) {
+            // return <Loading />;
+        }
+        
+        // ! Wth?
+        // const icons = [
+        //     { icon: "user", method: this.assignToUser },
+        //     { icon: "users", method: this.assignToWorkQueue },
+        //     { icon: "thumb-tack", method: this.assignAction }
+        // ];
+        
+        
+        return (
+            <div className="cc-container">
+                <div className={`left__side ${this.state.openPane ? '' : 'full__width'}`}>
+                    <TopBar />
+                </div>
+            </div>
+        )
+    }
+}
