27d05511c63ffb49791b9c8ef173d52f7dc9bbc1
MihaiPerju <mihaiperju98@gmail.com>
2018-11-18 14:09:05 +0000

    ARCC-345

--- app/imports/client/pages/clients/facilities/components/FacilityContent/PlacementBlock.jsx
+++ app/imports/client/pages/clients/facilities/components/FacilityContent/PlacementBlock.jsx
@@ -1,74 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Notifier from "../../../../../lib/Notifier";
-import DropzoneComponent from 'react-dropzone-component';
-import ImportingRules from '../ImportingRules';
+import DropzoneComponent from "react-dropzone-component";
+import ImportingRules from "../ImportingRules";
 import { getToken } from "/imports/api/uploads/utils";
 import DatePicker from "react-datepicker";
 import moment from "moment";
 
 export default class PlacementBlock extends Component {
+  constructor() {
+    super();
+    this.state = {
+      placementDate: new Date()
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-          placementDate: new Date()
-        };
-      }
+  onDateSelect = selectedDate => {
+    this.setState({ placementDate: selectedDate });
+  };
 
-    onDateSelect = selectedDate => {
-        this.setState({ placementDate: selectedDate });
-      };
+  render() {
+    const { facility, setTempRules } = this.props;
+    const { placementDate } = this.state;
+    const componentConfig = {
+      postUrl: `/uploads/csv/${facility._id}/${getToken()}`
+    };
 
-    render() {
-        const {facility, setTempRules} = this.props;
-        const {placementDate} = this.state;
-        const componentConfig = {
-            postUrl: `/uploads/csv/${facility._id}/${getToken()}`
-        };
+    const djsConfig = {
+      params: {
+        placementDate: this.state.placementDate
+      },
+      complete(file) {
+        Notifier.success("Added");
+        this.removeFile(file);
+      },
+      acceptedFiles: ".csv,.txt,.xls"
+    };
 
-        const djsConfig = {
-            params: {
-                placementDate: this.state.placementDate
-            },
-            complete(file) {
-                Notifier.success('Added');
-                this.removeFile(file);
-            },
-            acceptedFiles: '.csv'
-        };
-
-        return (
-            <div className="action-block drop-file">
-                <div className="header__block">
-                    <div className="title-block text-uppercase">Placement file</div>
-                </div>
-                <div className="upload-section">
-                    <div className="radio-group flex--helper flex-align--center">
-                        <label>Account Placement Date:</label>
-                        <DatePicker
-                        calendarClassName="cc-datepicker"
-                        showMonthDropdown
-                        showYearDropdown
-                        todayButton={"Today"}
-                        placeholderText="Account Placement Date"
-                        selected={moment(placementDate)}
-                        name="placementDate"
-                        onChange={date =>
-                            this.onDateSelect(date, "placementDate")
-                        }
-                        />
-                    </div>
-                  </div>
-                <div className="main__block">
-                    <div className="add-content">
-                        <i className="icon-upload"/>
-                        <DropzoneComponent config={componentConfig} djsConfig={djsConfig}/>
-                    </div>
-                </div>
-                <div className="upload-section">
-                    <ImportingRules rules={"placementRules"} model={facility} setTempRules={setTempRules}/>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="action-block drop-file">
+        <div className="header__block">
+          <div className="title-block text-uppercase">Placement file</div>
+        </div>
+        <div className="upload-section">
+          <div className="radio-group flex--helper flex-align--center">
+            <label>Account Placement Date:</label>
+            <DatePicker
+              calendarClassName="cc-datepicker"
+              showMonthDropdown
+              showYearDropdown
+              todayButton={"Today"}
+              placeholderText="Account Placement Date"
+              selected={moment(placementDate)}
+              name="placementDate"
+              onChange={date => this.onDateSelect(date, "placementDate")}
+            />
+          </div>
+        </div>
+        <div className="main__block">
+          <div className="add-content">
+            <i className="icon-upload" />
+            <DropzoneComponent config={componentConfig} djsConfig={djsConfig} />
+          </div>
+        </div>
+        <div className="upload-section">
+          <ImportingRules
+            rules={"placementRules"}
+            model={facility}
+            setTempRules={setTempRules}
+          />
+        </div>
+      </div>
+    );
+  }
 }
