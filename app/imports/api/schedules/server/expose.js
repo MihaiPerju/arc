0028fb3197d9fb9bbe7be9133e131b16f0bcabd8
MihaiPerju <mihaiperju98@gmail.com>
2018-11-29 05:26:02 +0000

    removed grapher on schedules

--- app/imports/api/schedules/server/expose.js
+++
@@ -1,5 +0,0 @@
-import schedulesQuery from '/imports/api/schedules/queries/scheduleList';
-import Schedules from '/imports/api/schedules/collection';
-
-Schedules.expose({});
-schedulesQuery.expose({});
