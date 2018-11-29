4fc4b717e431f1c71fb5466ea06bd4a193d19935
MihaiPerju <mihaiperju98@gmail.com>
2018-11-29 03:03:58 +0000

    removed graper on Notifications

--- app/imports/api/notifications/server/methods.js
+++ app/imports/api/notifications/server/methods.js
@@ -4,9 +4,16 @@ Meteor.methods({
   "notification.create"(data) {
     Notifications.insert(data);
   },
+
   "notification.remove"(_id) {
     Notifications.remove({ _id });
   },
+
+  "notifications.get"(filters) {
+    let options = { sort: { createdAt: -1 } };
+    return Notifications.find(filters, options).fetch();
+  },
+
   "notification.setAsSeen"(_id) {
     Notifications.update(
       { _id },
