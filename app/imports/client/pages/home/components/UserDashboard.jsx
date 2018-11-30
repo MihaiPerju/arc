45481ca4ec8c01b047f7c8d3c9b162449624eec3
intellectyx data science india pvt ltd <arun_s@intellectyx.com>
2018-11-30 12:43:57 +0530

    Implemented User Preferences of dashboard widgets on User Dashboard.

--- app/imports/client/pages/home/components/UserDashboard.jsx
+++ app/imports/client/pages/home/components/UserDashboard.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Notifier from "../../../lib/Notifier";
 import AssignedToMe from "./dashboardWidgets/AssignedToMe";
 import CompletedAccounts from "./dashboardWidgets/CompletedAccounts";
+import CloseAssists from './dashboardWidgets/CloseAssists';
+import ClosedAccounts from './dashboardWidgets/ClosedAccounts';
+import CollectedToday from './dashboardWidgets/CollectedToday';
 import pages from "../../../../api/settings/enums/settings";
 import Loading from "../../../lib/ui/Loading";
 
@@ -36,6 +39,9 @@ export default class UserDashboard extends React.Component {
         <div className="m-b--25">
           {widgetSettings.assignedToMe && <AssignedToMe filters={filters} />}
           {widgetSettings.completed && <CompletedAccounts filters={filters} />}
+          {widgetSettings.collectedToday && <CollectedToday filters={filters} />}
+          {widgetSettings.closedAccounts && <ClosedAccounts filters={filters} />}
+          {widgetSettings.closeAssists && <CloseAssists filters={filters} />}
         </div>
       );
     }
