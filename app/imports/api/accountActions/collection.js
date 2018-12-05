12ba1e160603c81e1a4c8b57f6de41957762c5f9
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2018-12-05 14:34:56 -0600

    Add index to actions collection

--- app/imports/api/accountActions/collection.js
+++ app/imports/api/accountActions/collection.js
@@ -2,6 +2,10 @@ import AccountActionsSchema from './schema.js'
 
 const AccountActions = new Mongo.Collection('account_actions');
 
+if(Meteor.isServer) {
+    AccountActions._ensureIndex({type: 1, clientId: 1, createdAt: 1});
+}
+
 AccountActions.attachSchema(AccountActionsSchema);
 
 export default AccountActions;
