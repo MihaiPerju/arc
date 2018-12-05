6e2abfef121315c68376910a2f6eb879592b8c97
MihaiPerju <mihaiperju98@gmail.com>
2018-12-05 14:01:14 +0000

    changed optionality

--- app/imports/api/facilities/schemas/PaymentRulesSchema.js
+++ app/imports/api/facilities/schemas/PaymentRulesSchema.js
@@ -1,79 +1,79 @@
-import SimpleSchema from 'simpl-schema';
-import insuranceSchema from './insuranceSchema';
+import SimpleSchema from "simpl-schema";
+import insuranceSchema from "./insuranceSchema";
 
 export default new SimpleSchema({
-    hasHeader: {
-        type: Boolean,
-        allowedValues: [true, false]
-    },
-    ptName: {
-        label: "Pacient Name",
-        type: String,
-        optional: true
-    },
-    acctNum: {
-        label: "Account Number",
-        type: String
-    },
-    admitDate: {
-        label: "Admit Date",
-        type: String,
-        optional: true
-    },
-    dischrgDate: {
-        label: "Discharge Date",
-        type: String,
-        optional: true
-    },
-    payorCode: {
-        label: "Payor Code",
-        type: String
-    },
-    insName: {
-        label: "Insurance Name",
-        type: String,
-        optional: true
-    },
-    newAcctBal: {
-        label: "New Account Balance",
-        type: String,
-        optional: true
-    },
-    ptBal: {
-        label: "Pacient Balance",
-        type: String,
-        optional: true
-    },
-    newInsBal: {
-        label: "New Insurance Balance",
-        type: Array,
-        optional: true
-    },
-    'newInsBal.$': {
-        label: "Insurance Balance",
-        type: insuranceSchema.omit('insName', 'insCode')
-    },
-    finClass: {
-        label: "Financial Class",
-        type: String,
-        optional: true
-    },
-    transDate: {
-        label: "Transaction Date",
-        type:
-        String
-    }
-    ,
-    transType: {
-        label: "Transaction Type",
-        type:
-        String
-    }
-    ,
-    transAmount: {
-        label: "Transaction Amount",
-        type:
-        String
-    }
-})
-;
+  hasHeader: {
+    type: Boolean,
+    allowedValues: [true, false]
+  },
+  ptName: {
+    label: "Pacient Name",
+    type: String,
+    optional: true
+  },
+  acctNum: {
+    label: "Account Number",
+    type: String,
+    optional: true
+  },
+  admitDate: {
+    label: "Admit Date",
+    type: String,
+    optional: true
+  },
+  dischrgDate: {
+    label: "Discharge Date",
+    type: String,
+    optional: true
+  },
+  payorCode: {
+    label: "Payor Code",
+    type: String,
+    optional: true
+  },
+  insName: {
+    label: "Insurance Name",
+    type: String,
+    optional: true
+  },
+  newAcctBal: {
+    label: "New Account Balance",
+    type: String,
+    optional: true
+  },
+  ptBal: {
+    label: "Pacient Balance",
+    type: String,
+    optional: true
+  },
+  newInsBal: {
+    label: "New Insurance Balance",
+    type: Array,
+    optional: true
+  },
+  "newInsBal.$": {
+    label: "Insurance Balance",
+    type: insuranceSchema.omit("insName", "insCode"),
+    optional: true
+  },
+  finClass: {
+    label: "Financial Class",
+    type: String,
+    optional: true
+  },
+  transDate: {
+    label: "Transaction Date",
+    type: String,
+    optional: true
+  },
+  transType: {
+    label: "Transaction Type",
+    type: String,
+    optional: true
+  },
+  transAmount: {
+    label: "Transaction Amount",
+    type: String,
+    optional: true
+  }
+});
