3c04c12e02574449e36854d1171484bb1c8f7036
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2018-12-05 14:48:55 -0600

    Add account id index to actions

--- app/imports/api/accountActions/collection.js
+++ app/imports/api/accountActions/collection.js
@@ -4,6 +4,7 @@ const AccountActions = new Mongo.Collection('account_actions');
 
 if(Meteor.isServer) {
     AccountActions._ensureIndex({type: 1, clientId: 1, createdAt: 1});
+    AccountActions._ensureIndex({accountId: 1});
 }
 
 AccountActions.attachSchema(AccountActionsSchema);
