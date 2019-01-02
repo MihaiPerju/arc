79778d7d4ae0d71682bc7c8247748657e2d6b47c
MihaiPerju <mihaiperju98@gmail.com>
2019-01-02 00:09:41 +0000

    style fixed

--- app/imports/client/pages/accounts/components/AccountContent/NewLetter.jsx
+++ app/imports/client/pages/accounts/components/AccountContent/NewLetter.jsx
@@ -1,34 +1,34 @@
-import React, {Component} from 'react';
-import {getImagePath} from '/imports/api/utils';
-import {AutoForm, SelectField} from 'uniforms-unstyled';
-import SimpleSchema from 'simpl-schema';
-import LetterCreateContainer
-  from '/imports/client/pages/letters/LetterCreateContainer.jsx';
+import React, { Component } from "react";
+import { getImagePath } from "/imports/api/utils";
+import { AutoForm, SelectField } from "uniforms-unstyled";
+import SimpleSchema from "simpl-schema";
+import LetterCreateContainer from "/imports/client/pages/letters/LetterCreateContainer.jsx";
+import SelectSimple from "/imports/client/lib/uniforms/SelectSimple.jsx";
 
 export default class NewLetter extends Component {
-  constructor () {
-    super ();
+  constructor() {
+    super();
     this.state = {
       fade: false,
-      selectedTemplate: {},
+      selectedTemplate: {}
     };
   }
 
-  componentDidMount () {
-    setTimeout (() => {
-      this.setState ({fade: true});
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ fade: true });
     }, 1);
   }
 
   getOptions = data => {
-    return _.map (data, letterTemplate => ({
+    return _.map(data, letterTemplate => ({
       value: letterTemplate._id,
-      label: letterTemplate.name,
+      label: letterTemplate.name
     }));
   };
 
-  getLetterTemplate (value) {
-    const {letterTemplates} = this.props;
+  getLetterTemplate(value) {
+    const { letterTemplates } = this.props;
     for (let letterTemplate of letterTemplates) {
       if (letterTemplate._id === value) {
         return letterTemplate;
@@ -36,47 +36,42 @@ export default class NewLetter extends Component {
     }
   }
 
-  onHandleChange (label, value) {
-    const selectedTemplate = this.getLetterTemplate (value);
-    this.setState ({
-      selectedTemplate,
+  onHandleChange(label, value) {
+    const selectedTemplate = this.getLetterTemplate(value);
+    this.setState({
+      selectedTemplate
     });
   }
 
-  cancel () {
-    const {cancel} = this.props;
-    cancel ();
+  cancel() {
+    const { cancel } = this.props;
+    cancel();
   }
 
-  render () {
-    const {letterTemplates, account, cancel} = this.props;
-    const {selectedTemplate} = this.state;
-    const {avatar, profile} = Meteor.user ();
-    const options = this.getOptions (letterTemplates);
-    
+  render() {
+    const { letterTemplates, account, cancel } = this.props;
+    const { selectedTemplate } = this.state;
+    const { avatar, profile } = Meteor.user();
+    const options = this.getOptions(letterTemplates);
+
     return (
-      <div className={this.state.fade ? 'new-letter in' : 'new-letter'}>
+      <div className={this.state.fade ? "new-letter in" : "new-letter"}>
         <div className="row-block">
           <div className="info">
             <img
               className="md-avatar img-circle"
-              src={
-                avatar ? getImagePath (avatar.path) : '/assets/img/user1.svg'
-              }
+              src={avatar ? getImagePath(avatar.path) : "/assets/img/user1.svg"}
               alt=""
             />
             <div className="name truncate">
-              {profile.firstName + ' ' + profile.lastName}
+              {profile.firstName + " " + profile.lastName}
             </div>
           </div>
           <div className="action-form">
-            <AutoForm
-              onChange={this.onHandleChange.bind (this)}
-              schema={schema}
-            >
+            <AutoForm onChange={this.onHandleChange.bind(this)} schema={schema}>
               <div className="select-row">
                 <div className="select-group">
-                  <SelectField
+                  <SelectSimple
                     className="select-width"
                     name="selectedOption"
                     placeholder="Select letter"
@@ -84,29 +79,32 @@ export default class NewLetter extends Component {
                   />
                 </div>
               </div>
-              <div className="btn-group">
-                <button onClick={this.cancel.bind (this)} className="btn--red">
-                  Cancel
-                </button>
-              </div>
+              <button
+                onClick={this.cancel.bind(this)}
+                style={{ margin: "0" }}
+                className="btn--red"
+              >
+                Cancel
+              </button>
             </AutoForm>
           </div>
         </div>
-        {!_.isEmpty (selectedTemplate) &&
+        {!_.isEmpty(selectedTemplate) && (
           <LetterCreateContainer
             selectedTemplate={selectedTemplate}
             account={account}
             reset={cancel}
             data={letterTemplates}
-          />}
+          />
+        )}
       </div>
     );
   }
 }
 
-const schema = new SimpleSchema ({
+const schema = new SimpleSchema({
   selectedOption: {
     type: String,
-    label: false,
-  },
+    label: false
+  }
 });
